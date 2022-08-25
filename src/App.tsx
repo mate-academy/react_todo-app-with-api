1600e65b089df57d123e93c4a1877ca122dcd400
PavloMarkov <r9markovpn@gmail.com>
2022-08-25 18:42:12 +0300

    adding, removing & change status todo were emplemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,56 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { useContext, useEffect, useRef } from 'react';
+import classNames from 'classnames';
+import React, {
+  useContext, useEffect, useRef, useState,
+} from 'react';
+import { getTodos } from './api/todos';
 import { AuthContext } from './components/Auth/AuthContext';
+import { SortType } from './types/SortType';
+import { Todo } from './types/Todo';
+import { client } from './utils/fetchClient';
 
 export const App: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const user = useContext(AuthContext);
   const newTodoField = useRef<HTMLInputElement>(null);
 
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputDataOfTodo, setInputDataOfTodo] = useState('');
+  const [newTodo, setNewTodo] = useState('');
+  const [sortType, setSortType] = useState(SortType.all);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     // focus the element with `ref={newTodoField}`
     if (newTodoField.current) {
       newTodoField.current.focus();
     }
-  }, []);
+
+    if (user) {
+      getTodos(user.id)
+        .then(res => setTodos(res.map(todo => ({
+          id: todo.id,
+          userId: todo.userId,
+          title: todo.title,
+          completed: todo.completed,
+        }))))
+        .catch(e => `Error is ${e}`);
+    }
+  }, [isLoading]);
+
+  const preparedTodosToShow = () => {
+    switch (sortType) {
+      case SortType.completed:
+        return todos.filter(todo => todo.completed === true);
+
+      case SortType.active:
+        return todos.filter(todo => todo.completed === false);
+
+      case SortType.all:
+      default:
+        return todos;
+    }
+  };
 
   return (
     <div className="todoapp">
@@ -26,19 +64,102 @@ export const App: React.FC = () => {
             className="todoapp__toggle-all active"
           />
 
-          <form>
+          <form
+            onSubmit={(event) => event.preventDefault()}
+          >
             <input
               data-cy="NewTodoField"
               type="text"
               ref={newTodoField}
               className="todoapp__new-todo"
               placeholder="What needs to be done?"
+              value={inputDataOfTodo}
+              onChange={(event) => {
+                setInputDataOfTodo(event.target.value);
+              }}
+              onKeyUp={(event) => {
+                if (event.key === 'Enter' && user && !!inputDataOfTodo) {
+                  setNewTodo(inputDataOfTodo);
+                  client.post<Todo>('/todos', {
+                    title: newTodo,
+                    userId: user.id,
+                    completed: false,
+                  })
+                    .then(res => {
+                      const todoFromServer: Todo = {
+                        id: res.id,
+                        userId: res.userId,
+                        title: res.title,
+                        completed: res.completed,
+                      };
+
+                      setTodos(prev => [...prev, todoFromServer]);
+                    })
+                    .catch(e => `error is ${e}`)
+                    .finally(() => {
+                      setNewTodo('');
+                      setInputDataOfTodo('');
+                    });
+                }
+              }}
             />
           </form>
         </header>
 
         <section className="todoapp__main" data-cy="TodoList">
-          <div data-cy="Todo" className="todo completed">
+          {preparedTodosToShow().map(todo => (
+            <div
+              data-cy="Todo"
+              key={todo.id}
+              className={classNames('todo', { completed: todo.completed })}
+            >
+              <label className="todo__status-label">
+                <input
+                  data-cy="TodoStatus"
+                  type="checkbox"
+                  className="todo__status"
+                  defaultChecked={todo.completed}
+                  onClick={() => {
+                    setIsLoading(true);
+                    client.patch<Todo>(`/todos/${todo.id}`, {
+                      completed: !todo.completed,
+                    })
+                    // .then(res => {
+                    //   const newTodo = todos.find(todoNew => (
+                    //     todoNew.id === res.id));
+
+                    // });
+                      .finally(() => setIsLoading(false));
+                  }}
+                />
+              </label>
+
+              <span
+                data-cy="TodoTitle"
+                className="todo__title"
+              >
+                {todo.title}
+              </span>
+              <button
+                type="button"
+                className="todo__remove"
+                data-cy="TodoDeleteButton"
+                onClick={() => {
+                  setIsLoading(false);
+                  client.delete(`/todos/${todo.id}`)
+                    .finally(() => setIsLoading(true));
+                }}
+              >
+                ×
+              </button>
+
+              <div data-cy="TodoLoader" className="modal overlay">
+                <div className="modal-background has-background-white-ter" />
+                <div className="loader" />
+              </div>
+            </div>
+          ))}
+          {/* <div data-cy="Todo" className="todo completed">
             <label className="todo__status-label">
               <input
                 data-cy="TodoStatus"
@@ -159,19 +280,22 @@ export const App: React.FC = () => {
               <div className="modal-background has-background-white-ter" />
               <div className="loader" />
             </div>
-          </div>
+          </div> */}
         </section>
 
         <footer className="todoapp__footer" data-cy="Footer">
           <span className="todo-count" data-cy="todosCounter">
-            4 items left
+            {`${todos.length} items left`}
           </span>
 
           <nav className="filter" data-cy="Filter">
             <a
               data-cy="FilterLinkAll"
               href="#/"
-              className="filter__link selected"
+              className={classNames(
+                'filter__link', { selected: sortType === SortType.all },
+              )}
+              onClick={() => setSortType(SortType.all)}
             >
               All
             </a>
@@ -179,14 +303,20 @@ export const App: React.FC = () => {
             <a
               data-cy="FilterLinkActive"
               href="#/active"
-              className="filter__link"
+              className={classNames(
+                'filter__link', { selected: sortType === SortType.active },
+              )}
+              onClick={() => setSortType(SortType.active)}
             >
               Active
             </a>
             <a
               data-cy="FilterLinkCompleted"
               href="#/completed"
-              className="filter__link"
+              className={classNames(
+                'filter__link', { selected: sortType === SortType.completed },
+              )}
+              onClick={() => setSortType(SortType.completed)}
             >
               Completed
             </a>
--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -4,3 +4,6 @@ export interface Todo {
   title: string;
   completed: boolean;
 }
+
+export type TodoToAdd = Pick<Todo, 'userId' | 'title' | 'completed' >;
+export type CompletedTodoChange = Pick<Todo, 'completed' >;
