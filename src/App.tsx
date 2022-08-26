4a8b453207dd703f395bf9b4236ae78cbca90c1e
Kate <krasnova2912@gmail.com>
2022-08-26 20:11:27 +0300

    add and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
 import { AuthContext } from './components/Auth/AuthContext';
+import { Todo } from './types/Todo';
+import { addTodo, getTodos, removeTodo } from './api/todos';
 
-export const App: React.FC = () => {
+type ErrorStr = (
+  'Unable to add a todo'
+  | 'Unable to delete a todo'
+  | 'Unable to update a todo'
+  | "Title can't be empty"
+  );
+
+export const App: React.FC<{}> = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const user = useContext(AuthContext);
   const newTodoField = useRef<HTMLInputElement>(null);
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState<ErrorStr | null>(null);
+  const [loadingTodo, setLoadingTodo] = useState<number | null>(null);
+  // const [isComplited, setIsComplited] =
+
+  useEffect(() => {
+    if (user) {
+      getTodos(user.id)
+        .then(res => setTodos(res));
+    }
+  }, []);
 
   useEffect(() => {
     // focus the element with `ref={newTodoField}`
@@ -13,6 +36,43 @@ export const App: React.FC = () => {
       newTodoField.current.focus();
     }
   }, []);
+
+  const errorMsg = (msg: ErrorStr) => {
+    setError(msg);
+    setTimeout(() => setError(null), 3000);
+  };
+
+  const handleSubmit = () => {
+    if (query === '') {
+      errorMsg("Title can't be empty");
+
+      return;
+    }
+
+    if (user) {
+      const newTodo = {
+        title: query,
+        userId: user.id,
+        completed: false,
+      };
+
+      addTodo(newTodo)
+        .then(res => setTodos((prev) => [...prev, res]))
+        .catch(() => errorMsg('Unable to add a todo'))
+        .finally(() => setQuery(''));
+    }
+  };
+
+  const deleteTodo = (id: number) => {
+    setLoadingTodo(id);
+
+    removeTodo(id)
+      .then(() => {
+        setTodos(todos.filter(todo => todo.id !== id));
+      })
+      .catch(() => errorMsg('Unable to delete a todo'))
+      .finally(() => setLoadingTodo(null));
+  };
 
   return (
     <div className="todoapp">
@@ -26,44 +86,70 @@ export const App: React.FC = () => {
             className="todoapp__toggle-all active"
           />
 
-          <form>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+          >
             <input
               data-cy="NewTodoField"
               type="text"
               ref={newTodoField}
               className="todoapp__new-todo"
               placeholder="What needs to be done?"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
             />
           </form>
         </header>
 
         <section className="todoapp__main" data-cy="TodoList">
-          <div data-cy="Todo" className="todo completed">
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-                defaultChecked
-              />
-            </label>
-
-            <span data-cy="TodoTitle" className="todo__title">HTML</span>
-            <button
-              type="button"
-              className="todo__remove"
-              data-cy="TodoDeleteButton"
+          {todos.map(todo => (
+            <div
+              data-cy="Todo"
+              className="todo completed"
+              key={todo.id}
             >
-              ×
-            </button>
+              <label className="todo__status-label">
+                <input
+                  data-cy="TodoStatus"
+                  type="checkbox"
+                  className="todo__status"
+                  checked={todo.completed}
+                  onClick={handleComplite}
+                />
+              </label>
 
-            <div data-cy="TodoLoader" className="modal overlay">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
+              <span
+                data-cy="TodoTitle"
+                className="todo__title"
+              >
+                {todo.title}
+              </span>
+              <button
+                type="button"
+                className="todo__remove"
+                data-cy="TodoDeleteButton"
+                onClick={() => deleteTodo(todo.id)}
+              >
+                ×
+              </button>
+
+              <div
+                data-cy="TodoLoader"
+                className={
+                  classNames(
+                    'modal overlay', { 'is-active': loadingTodo === todo.id },
+                  )
+                }
+              >
+                <div className="modal-background has-background-white-ter" />
+                <div className="loader" />
+              </div>
             </div>
-          </div>
+          ))}
 
-          <div data-cy="Todo" className="todo">
+          {/* <div data-cy="Todo" className="todo">
             <label className="todo__status-label">
               <input
                 data-cy="TodoStatus"
@@ -159,12 +245,12 @@ export const App: React.FC = () => {
               <div className="modal-background has-background-white-ter" />
               <div className="loader" />
             </div>
-          </div>
+          </div> */}
         </section>
 
         <footer className="todoapp__footer" data-cy="Footer">
           <span className="todo-count" data-cy="todosCounter">
-            4 items left
+            {`${todos.length} items left`}
           </span>
 
           <nav className="filter" data-cy="Filter">
@@ -202,22 +288,26 @@ export const App: React.FC = () => {
         </footer>
       </div>
 
-      <div
-        data-cy="ErrorNotification"
-        className="notification is-danger is-light has-text-weight-normal"
-      >
-        <button
-          data-cy="HideErrorButton"
-          type="button"
-          className="delete"
-        />
+      {error && (
+        <div
+          data-cy="ErrorNotification"
+          className="notification is-danger is-light has-text-weight-normal"
+        >
+          <button
+            data-cy="HideErrorButton"
+            type="button"
+            className="delete"
+            onClick={() => setError(null)}
+          />
 
-        Unable to add a todo
-        <br />
-        Unable to delete a todo
-        <br />
-        Unable to update a todo
-      </div>
+          {/* Unable to add a todo
+          <br />
+          Unable to delete a todo
+          <br />
+          Unable to update a todo */}
+          {error}
+        </div>
+      )}
     </div>
   );
 };
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,4 +1,4 @@
-import { Todo } from '../types/Todo';
+import { NewTodo, Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
 export const getTodos = (userId: number) => {
@@ -6,3 +6,11 @@ export const getTodos = (userId: number) => {
 };
 
 // Add more methods here
+
+export const addTodo = (newTodo: NewTodo): Promise<Todo> => {
+  return client.post('/todos', newTodo);
+};
+
+export const removeTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
