5f9853ff2c9517a65618cd407fccfa2acc959dd6
Nikita Biriukov <flimsy00@gmail.com>
2022-08-31 20:30:54 +0300

    solution

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,172 @@
+import { Todo } from '../types/Todo';
+import { AddedTodoItem } from './AddedTodoItem';
+import { TodoItem } from './TodoItem';
+
+type Props = {
+  todos: Todo[];
+  removeTodo: (todoId: number) => void;
+  selectedTodoId: number | null;
+  isLoading: boolean;
+  title: string;
+  isRemoveLoading: boolean;
+  updateTodoById: (todoId: number, data: {}) => void;
+  isUpdateLoading: boolean;
+  isAllToggled: boolean;
+};
+
+export const TodoList: React.FC<Props> = (props) => {
+  const {
+    todos,
+    removeTodo,
+    selectedTodoId,
+    isLoading,
+    title,
+    isRemoveLoading,
+    updateTodoById,
+    isUpdateLoading,
+    isAllToggled,
+  } = props;
+
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => {
+        return (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            removeTodo={removeTodo}
+            selectedTodoId={selectedTodoId}
+            isRemoveLoading={isRemoveLoading}
+            updateTodoById={updateTodoById}
+            isUpdateLoading={isUpdateLoading}
+            isAllToggled={isAllToggled}
+          />
+        );
+      })}
+
+      {isLoading && <AddedTodoItem title={title} />}
+      {/* <div data-cy="Todo" className="todo completed">
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+            defaultChecked
+          />
+        </label>
+
+        <span data-cy="TodoTitle" className="todo__title">HTML</span>
+        <button
+          type="button"
+          className="todo__remove"
+          data-cy="TodoDeleteButton"
+        >
+          ×
+        </button>
+
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+
+      <div data-cy="Todo" className="todo">
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+          />
+        </label>
+
+        <span data-cy="TodoTitle" className="todo__title">CSS</span>
+
+        <button
+          type="button"
+          className="todo__remove"
+          data-cy="TodoDeleteButton"
+        >
+          ×
+        </button>
+
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+
+      <div data-cy="Todo" className="todo">
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+          />
+        </label>
+
+        <form>
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            defaultValue="JS"
+          />
+        </form>
+
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+
+      <div data-cy="Todo" className="todo">
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+          />
+        </label>
+
+        <span data-cy="TodoTitle" className="todo__title">React</span>
+        <button
+          type="button"
+          className="todo__remove"
+          data-cy="TodoDeleteButton"
+        >
+          ×
+        </button>
+
+        <div data-cy="TodoLoader" className="modal overlay">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+
+      <div data-cy="Todo" className="todo">
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+          />
+        </label>
+
+        <span data-cy="TodoTitle" className="todo__title">Redux</span>
+        <button
+          type="button"
+          className="todo__remove"
+          data-cy="TodoDeleteButton"
+        >
+          ×
+        </button>
+
+        <div data-cy="TodoLoader" className="modal overlay is-active">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div> */}
+    </section>
+  );
+};
