6571c31407733f01be2b25ff8620ff247cc83f03
Yura Lysenko <yuralysenko1@gmail.com>
2022-08-31 10:52:35 +0300

    initial commit

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,14 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const createTodo = (userId: number, title: string) => {
+  return client.post<Todo>('/todos', { userId, title, completed: false });
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updateTodo = (todoId: number, data: any) => {
+  return client.patch<Todo>(`/todos/${todoId}`, data);
+};
