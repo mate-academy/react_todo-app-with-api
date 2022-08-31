7e35813000d44d13b639908076a347a507553585
Denys Korotkoruchko <denenergii@gmail.com>
2022-08-31 13:55:03 +0300

    ready

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,19 @@
 import { Todo } from '../types/Todo';
+import { NewTodo } from '../types/NewTodo';
 import { client } from '../utils/fetchClient';
 
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const postTodo = (todo: NewTodo) => {
+  return client.post('/todos', todo);
+};
+
+export const patchTodo = (todoId: number, todo: Todo) => {
+  return client.patch(`/todos/${todoId}`, todo);
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
