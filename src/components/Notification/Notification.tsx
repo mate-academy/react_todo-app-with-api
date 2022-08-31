a24e1e139235156de826c579673137da3197a89a
Oleksìi <bardyshev@gmail.com>
2022-08-31 11:51:10 +0300

    app was split into components

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Notification = () => {
+  return (
+    <div>
+      <div
+        data-cy="ErrorNotification"
+        className="notification is-danger is-light has-text-weight-normal"
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          aria-label="Delete"
+        />
+
+        Unable to add a todo
+        <br />
+        Unable to delete a todo
+        <br />
+        Unable to update a todo
+      </div>
+    </div>
+  );
+};
