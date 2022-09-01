651bfecb2a553bad25a12502298a2a6b9b669c9d
Oleh Butenko <6083341@gmail.com>
2022-09-01 12:16:51 +0300

    done

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,4 +5,32 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const createTodo = (title: string, userId: number) => {
+  return client.post<Todo>(
+    '/todos',
+    {
+      title,
+      userId,
+      completed: false,
+      isLoading: false,
+    },
+  );
+};
+
+export const deleteTodo = (todoId: number) => {
+  const url = `/todos/${todoId}`;
+
+  return client.delete(url);
+};
+
+export const changeStatus = (
+  id: number,
+  changes: Partial<Omit<Todo, 'id'>>,
+) => {
+  const url = `/todos/${id}`;
+
+  return client.patch<Todo>(
+    url,
+    changes,
+  );
+};
