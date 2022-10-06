f583a9ee9f1885f027e1584807685228a2d82e1d
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-10-06 13:34:39 +0300

    smooth animations was implemented

--- src/components/todos/TodoList/TodoList.tsx
+++ src/components/todos/TodoList/TodoList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Todo } from '../../../types/Todo';
 import { TodoItem } from '../TodoItem';
 
@@ -23,41 +24,54 @@ export const TodoList: React.FC<Props> = ({
 }) => {
   return (
     <section className="todoapp__main" data-cy="TodoList">
+      <TransitionGroup>
 
-      {todos.map(({
-        id,
-        title,
-        completed,
-      }) => {
-        return (
-          <TodoItem
-            key={id}
-            id={id}
-            title={title}
-            completed={completed}
-            removeTodo={removeTodo}
-            isAdding={false}
-            loadingTodos={loadingTodos}
-            setLoadingTodos={setLoadingTodos}
-            toggleTodoStatus={toggleTodoStatus}
-            todoRenaming={todoRenaming}
-          />
-        );
-      })}
-      {tempTodo && (
-        <TodoItem
-          id={tempTodo.id}
-          title={tempTodo.title}
-          completed={tempTodo.completed}
-          removeTodo={removeTodo}
-          isAdding
-          loadingTodos={loadingTodos}
-          setLoadingTodos={setLoadingTodos}
-          toggleTodoStatus={toggleTodoStatus}
-          todoRenaming={todoRenaming}
-        />
-      )}
-
+        {todos.map(({
+          id,
+          title,
+          completed,
+        }) => {
+          return (
+            <CSSTransition
+              key={id}
+              timeout={300}
+              classNames="item"
+            >
+              <TodoItem
+                key={id}
+                id={id}
+                title={title}
+                completed={completed}
+                removeTodo={removeTodo}
+                isAdding={false}
+                loadingTodos={loadingTodos}
+                setLoadingTodos={setLoadingTodos}
+                toggleTodoStatus={toggleTodoStatus}
+                todoRenaming={todoRenaming}
+              />
+            </CSSTransition>
+          );
+        })}
+        {tempTodo && (
+          <CSSTransition
+            key={0}
+            timeout={300}
+            classNames="temp-item"
+          >
+            <TodoItem
+              id={tempTodo.id}
+              title={tempTodo.title}
+              completed={tempTodo.completed}
+              removeTodo={removeTodo}
+              isAdding
+              loadingTodos={loadingTodos}
+              setLoadingTodos={setLoadingTodos}
+              toggleTodoStatus={toggleTodoStatus}
+              todoRenaming={todoRenaming}
+            />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </section>
   );
 };
