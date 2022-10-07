ceda9001c843aad9c7a420f3966608a793db752f
Yurii Pronin <ProninYurij@i.ua>
2022-10-07 13:50:15 +0300

    solutionReactTodoAppWithApi

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,23 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const getTodos = (userId: number) => {
+export const getTodos = (userId: number | null) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const createTodo = (userId: number | null, title: string) => {
+  return client.post<Todo>('/todos',
+    {
+      userId,
+      completed: false,
+      title,
+    });
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updateTodo = (todoId: number, data: Partial<Todo>) => {
+  return client.patch<Todo>(`/todos/${todoId}`, data);
+};
---
+++ src/components/Footer.tsx
@@ -0,0 +1,75 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Todo } from '../types/Todo';
+import { Filters } from '../types/Filters';
+
+type Props = {
+  setFilterBy: (arg: Filters) => void;
+  filterBy: Filters;
+  todos: Todo[] | null;
+  deleteTodo: () => void
+};
+
+export const Footer: React.FC<Props> = ({
+  filterBy,
+  todos,
+  setFilterBy,
+  deleteTodo,
+}) => {
+  const handleChangeFilterBy = (filteredBy: Filters) => {
+    setFilterBy(filteredBy);
+  };
+
+  const filterByParam = (param: string) => {
+    return filterBy === param;
+  };
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="todosCounter">
+        {`${todos?.length} items left`}
+
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        <a
+          data-cy="FilterLinkAll"
+          href="#/"
+          className={classNames('filter__link',
+            { selected: filterByParam('all') })}
+          onClick={() => handleChangeFilterBy(Filters.All)}
+        >
+          All
+        </a>
+
+        <a
+          data-cy="FilterLinkActive"
+          href="#/active"
+          className={classNames('filter__link',
+            { selected: filterByParam('active') })}
+          onClick={() => handleChangeFilterBy(Filters.Active)}
+        >
+          Active
+        </a>
+        <a
+          data-cy="FilterLinkCompleted"
+          href="#/completed"
+          className={classNames('filter__link',
+            { selected: filterByParam('completed') })}
+          onClick={() => handleChangeFilterBy(Filters.Completed)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      <button
+        data-cy="ClearCompletedButton"
+        type="button"
+        className="todoapp__clear-completed"
+        onClick={deleteTodo}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
