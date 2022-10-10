bd9166c61369936f0d3e4524bbe2e6fbcc888cfa
Roma Artiukh <romanskarb@ukr.net>
2022-10-10 09:05:01 +0300

    Solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -6,3 +6,20 @@ export const getTodos = (userId: number) => {
 };
 
 // Add more methods here
+
+export const createTodo = (title: string, userId: number) => {
+  return client.post<Todo>('/todos',
+    {
+      title,
+      userId,
+      completed: false,
+    });
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updateTodo = (
+  todoID: number, data: Partial<Todo>,
+): Promise<Todo> => client.patch<Todo>(`/todos/${todoID}`, data);
