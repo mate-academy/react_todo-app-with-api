4fcd2f78a106e4496ace19f1e732fef0988139ac
Dima Cherednyk <dicherednyk@gmail.com>
2022-10-11 19:30:33 +0300

    Solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,22 +1,22 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const getTodos = (userId: number) => {
+export const getTodos = async (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-export const addNewTodo = (todo: Todo) => {
+export const addNewTodo = async (todo: Todo) => {
   return client.post<Todo[]>('/todos', todo);
 };
 
-export const deleteTodo = (todoId: number) => {
+export const deleteTodo = async (todoId: number) => {
   return client.delete(`/todos/${todoId}`);
 };
 
-export const checkboxTodo = (todoId: number, completed: boolean) => {
+export const checkboxTodo = async (todoId: number, completed: boolean) => {
   return client.patch<Todo[]>(`/todos/${todoId}`, { completed });
 };
 
-export const changeTitles = (todoId: number, title: string) => {
+export const changeTitles = async (todoId: number, title: string) => {
   return client.patch<Todo[]>(`/todos/${todoId}`, { title });
 };
