a29f9d1a87eeb3f72683e28417d6934dbe8a954f
Synytsyn Denys <yungdena54@gmail.com>
2022-10-24 10:18:54 +0300

    added smooth aniamtions

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,3 +1,4 @@
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { TodoItem } from '../TodoItem/TodoItem';
 import { Todo } from '../../types/Todo';
 
@@ -23,18 +24,22 @@ export const TodoList: React.FC<Props> = ({
 }) => {
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          removeTodo={removeTodo}
-          handleUpdateTodo={handleUpdateTodo}
-          isAdding={isAdding}
-          toggleLoader={toggleLoader}
-          selectedId={selectedId}
-          completedIds={completedIds}
-        />
-      ))}
+      <TransitionGroup>
+        {todos.map((todo) => (
+          <CSSTransition key={todo.id} timeout={300} classNames="item">
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              handleUpdateTodo={handleUpdateTodo}
+              isAdding={isAdding}
+              toggleLoader={toggleLoader}
+              selectedId={selectedId}
+              completedIds={completedIds}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </section>
   );
 };
