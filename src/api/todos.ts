d350ec778c07c24a29168f0e3202a56ec643cb82
Sasha Shevchenko <sasha.shevchenko763@gmail.com>
2022-11-13 10:17:55 +0200

    added  todo app with api

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,21 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
+import { NewTodo } from '../types/NewTodo';
 
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const addTodo = ({ userId, title, completed }: NewTodo) => {
+  return client.post<Todo>('/todos', {
+    userId, title, completed,
+  });
+};
+
+export const deleteTodos = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updateTodos = (todoId: number, data: {}) => {
+  return client.patch(`/todos/${todoId}`, data);
+};
