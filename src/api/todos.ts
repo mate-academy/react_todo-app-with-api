8497f209ecbfa98029df9228545d56b4513d91c0
unknown <dsydorenko534@gmail.com>
2022-11-19 19:26:11 +0200

    refactor the code + fix

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,21 @@
 import { Todo } from '../types/Todo';
+import { TodoToAdd } from '../types/TodoToAdd';
 import { client } from '../utils/fetchClient';
 
-export const getTodos = (userId: number) => {
+export const getTodos = (userId: number): Promise<Todo[]> => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const addTodo = (newTodo: TodoToAdd): Promise<Todo> => {
+  return client.post<Todo>('/todos', newTodo);
+};
+
+export const deleteTodo = (todoId: number): Promise<any> => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updateTodo = (
+  todoId: number, changedField: object,
+): Promise<Todo> => {
+  return client.patch(`/todos/${todoId}`, changedField);
+};
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,136 @@
+import React, { useEffect, useRef, useState } from 'react';
+import cn from 'classnames';
+
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+  removeTodo: (todoId: number) => void;
+  editTodo: (todoId: number, fieldToChange: object) => Promise<void>;
+  loadingTodosIds: number[];
+};
+
+export const TodoItem: React.FC<Props> = React.memo(({
+  todo,
+  removeTodo,
+  editTodo,
+  loadingTodosIds,
+}) => {
+  const [titleQuery, setTitleQuery] = useState(todo.title);
+  const [isInputVisible, setIsInputVisible] = useState(false);
+
+  const titleChangeInput = useRef<HTMLInputElement>(null);
+  const { title, completed, id } = todo;
+
+  const handleDelete = () => {
+    removeTodo(id);
+  };
+
+  const handleTodoStatus = () => {
+    editTodo(id, { completed: !completed });
+  };
+
+  useEffect(() => {
+    if (titleChangeInput.current) {
+      titleChangeInput.current.focus();
+    }
+  }, [isInputVisible]);
+
+  const renameTodo = (newTitle: string) => {
+    editTodo(id, { title: newTitle });
+  };
+
+  const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleQuery(event.target.value);
+  };
+
+  const onTitleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (titleQuery === title) {
+      setIsInputVisible(false);
+
+      return;
+    }
+
+    if (titleQuery.trim()) {
+      renameTodo(titleQuery);
+      setIsInputVisible(false);
+    } else {
+      setTitleQuery('deleting ...');
+      removeTodo(id);
+    }
+  };
+
+  const onPressEscapeKey = (event: React.KeyboardEvent) => {
+    if (event.key === 'Escape' && isInputVisible) {
+      setIsInputVisible(false);
+    }
+  };
+
+  const handleOnBlur = () => {
+    setIsInputVisible(false);
+  };
+
+  const loaderIsActive = loadingTodosIds.includes(id);
+
+  return (
+    <div
+      data-cy="Todo"
+      className={cn('todo', { completed })}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          onChange={handleTodoStatus}
+        />
+      </label>
+
+      {isInputVisible ? (
+        <form onSubmit={onTitleFormSubmit}>
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            value={titleQuery}
+            onChange={handleChangeTitle}
+            onKeyDown={onPressEscapeKey}
+            ref={titleChangeInput}
+            onBlur={handleOnBlur}
+          />
+        </form>
+      ) : (
+        <>
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={() => setIsInputVisible(true)}
+          >
+            {title}
+          </span>
+
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDeleteButton"
+            onClick={handleDelete}
+          >
+            ×
+          </button>
+        </>
+      )}
+
+      <div
+        data-cy="TodoLoader"
+        className={cn('modal overlay',
+          { 'is-active': loaderIsActive })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+});
