f4ff7d293a1ad85ab6f1d0027d0d89132bc5a7e3
Matvii <admin@admins-MacBook-Air.local>
2022-11-26 23:28:47 +0100

    all errors handled

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,31 @@ import { ErrorContext } from './components/ErrorContext';
 
 export const App: React.FC = () => {
   const [hasLoadingError, setHasLoadingError] = useState(false);
-  const [isAddingErrorShown, setIsAddingErrorShown] = useState(false);
+  const [isEmptyTitleErrorShown, setIsEmptyTitleErrorShown] = useState(false);
   const [isTogglingErrorShown, setIsTogglingErrorShown] = useState(false);
   const [isRemoveErrorShown, setIsRemoveErrorShown] = useState(false);
+  const [isAddingErrorShown, setIsAddingErrorShown] = useState(false);
+
+  const errorObject = {
+    hasLoadingError,
+    setHasLoadingError,
+    isTogglingErrorShown,
+    setIsTogglingErrorShown,
+    isRemoveErrorShown,
+    setIsRemoveErrorShown,
+    isEmptyTitleErrorShown,
+    setIsEmptyTitleErrorShown,
+    isAddingErrorShown,
+    setIsAddingErrorShown,
+  };
 
   return (
     <ErrorContext.Provider
-      value={{
-        hasLoadingError,
-        setHasLoadingError,
-        isTogglingErrorShown,
-        setIsTogglingErrorShown,
-        isRemoveErrorShown,
-        setIsRemoveErrorShown,
-        isAddingErrorShown,
-        setIsAddingErrorShown,
-      }}
+      value={errorObject}
     >
       <div className="todoapp">
         <h1 className="todoapp__title">todos</h1>
-
         <TodoContent />
-
         <ErrorNotification />
       </div>
     </ErrorContext.Provider>
