a7eae71dc134f80ccad1688bd9fbd7a375f4cf6e
Kyrylo Haiduk <kirillhaiduk0@gmail.com>
2023-01-31 17:00:16 +0200

    added React.memo to header

--- src/components/TodoAppHeader/TodoAppHeader.tsx
+++ src/components/TodoAppHeader/TodoAppHeader.tsx
@@ -11,49 +11,51 @@ type Props = {
   allTodosCompleted: boolean;
 };
 
-export const TodoAppHeader: React.FC<Props> = ({
-  addNewTodo,
-  isAdding,
-  toggleTodosCompleted,
-  allTodosCompleted,
-}) => {
-  const [todoTitle, setTodoTitle] = useState('');
-  const newTodoField = useRef<HTMLInputElement>(null);
+export const TodoAppHeader: React.FC<Props> = React.memo(
+  ({
+    addNewTodo,
+    isAdding,
+    toggleTodosCompleted,
+    allTodosCompleted,
+  }) => {
+    const [todoTitle, setTodoTitle] = useState('');
+    const newTodoField = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    if (newTodoField.current) {
-      newTodoField.current.focus();
-    }
-  }, []);
+    useEffect(() => {
+      if (newTodoField.current) {
+        newTodoField.current.focus();
+      }
+    }, []);
 
-  const handleNewTodoFieldSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    addNewTodo(todoTitle.trim());
-    setTodoTitle('');
-  };
+    const handleNewTodoFieldSubmit = (event: React.FormEvent) => {
+      event.preventDefault();
+      addNewTodo(todoTitle.trim());
+      setTodoTitle('');
+    };
 
-  return (
-    <header className="todoapp__header">
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button
-        data-cy="ToggleAllButton"
-        type="button"
-        className={`todoapp__toggle-all ${allTodosCompleted && 'active'}`}
-        onClick={toggleTodosCompleted}
-      />
-
-      <form onSubmit={handleNewTodoFieldSubmit}>
-        <input
-          data-cy="NewTodoField"
-          type="text"
-          ref={newTodoField}
-          className="todoapp__new-todo"
-          placeholder="What needs to be done?"
-          value={todoTitle}
-          onChange={event => setTodoTitle(event.target.value)}
-          disabled={isAdding}
+    return (
+      <header className="todoapp__header">
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+        <button
+          data-cy="ToggleAllButton"
+          type="button"
+          className={`todoapp__toggle-all ${allTodosCompleted && 'active'}`}
+          onClick={toggleTodosCompleted}
         />
-      </form>
-    </header>
-  );
-};
+
+        <form onSubmit={handleNewTodoFieldSubmit}>
+          <input
+            data-cy="NewTodoField"
+            type="text"
+            ref={newTodoField}
+            className="todoapp__new-todo"
+            placeholder="What needs to be done?"
+            value={todoTitle}
+            onChange={event => setTodoTitle(event.target.value)}
+            disabled={isAdding}
+          />
+        </form>
+      </header>
+    );
+  },
+);
