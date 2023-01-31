dfd2dca10fcde57ec87d37f4958100798ae2c39d
Anastasiia Hordieieva <87222645+olenitut@users.noreply.github.com>
2023-01-31 18:25:47 +0200

    solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,24 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
+export type CustomTodo = {
+  userId: number;
+  title: string;
+  completed: boolean;
+};
+
 export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-// Add more methods here
+export const addTodo = (todo: CustomTodo) => {
+  return client.post<Todo>('/todos', todo);
+};
+
+export const deleteTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
+
+export const updateTodo = (id: number, data: Partial<Todo>) => {
+  return client.patch(`/todos/${id}`, data);
+};
