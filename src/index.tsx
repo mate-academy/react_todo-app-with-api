43cb19287762b2554fd0c84793af16b1703e476b
Misha Hrynko <misha@mate.academy>
2023-02-01 17:14:28 +0200

    remove initial code

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,6 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './styles/index.scss';
 
 import { App } from './App';
-import { AuthProvider } from './components/Auth/AuthContext';
-
-const Root = () => (
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-);
 
 createRoot(document.getElementById('root') as HTMLDivElement)
-  .render(<Root />);
+  .render(<App />);
