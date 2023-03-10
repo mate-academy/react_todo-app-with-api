d02edb69794d1ec058821bf6f0c6438c43c2e6c5
Denis Bokov <denisbokov1703@gmail.com>
2023-03-10 23:13:50 +0100

    Solution `no comments`

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -20,57 +20,57 @@ export const Footer: React.FC<Props> = React.memo(
     leftTodos,
   }) => (
 
-      <footer className="todoapp__footer">
-        <span className="todo-count">
-          {`${leftTodos} items left`}
-        </span>
+    <footer className="todoapp__footer">
+      <span className="todo-count">
+        {`${leftTodos} items left`}
+      </span>
 
-        <nav className="filter">
-          <a
-            href="#/"
-            className={classNames(
-              'filter__link',
-              { selected: filterBy === SortType.ALL },
-            )}
-            onClick={() => setFilterBy(SortType.ALL)}
-          >
-            {SortType.ALL}
-          </a>
-
-          <a
-            href="#/"
-            className={classNames(
-              'filter__link',
-              { selected: filterBy === SortType.ACTIVE },
-            )}
-            onClick={() => setFilterBy(SortType.ACTIVE)}
-          >
-            {SortType.ACTIVE}
-          </a>
-
-          <a
-            href="#/"
-            className={classNames(
-              'filter__link',
-              { selected: filterBy === SortType.COMPLETED },
-            )}
-            onClick={() => setFilterBy(SortType.COMPLETED)}
-          >
-            {SortType.COMPLETED}
-          </a>
-        </nav>
-
-        <button
-          type="button"
+      <nav className="filter">
+        <a
+          href="#/"
           className={classNames(
-            'todoapp__clear-completed',
-            { 'todoapp__clear-completed_none': !hasCompleteTodos },
-
+            'filter__link',
+            { selected: filterBy === SortType.ALL },
           )}
-          onClick={clearCompleted}
+          onClick={() => setFilterBy(SortType.ALL)}
         >
-          Clear completed
-        </button>
-      </footer>
-    )
-)
+          {SortType.ALL}
+        </a>
+
+        <a
+          href="#/"
+          className={classNames(
+            'filter__link',
+            { selected: filterBy === SortType.ACTIVE },
+          )}
+          onClick={() => setFilterBy(SortType.ACTIVE)}
+        >
+          {SortType.ACTIVE}
+        </a>
+
+        <a
+          href="#/"
+          className={classNames(
+            'filter__link',
+            { selected: filterBy === SortType.COMPLETED },
+          )}
+          onClick={() => setFilterBy(SortType.COMPLETED)}
+        >
+          {SortType.COMPLETED}
+        </a>
+      </nav>
+
+      <button
+        type="button"
+        className={classNames(
+          'todoapp__clear-completed',
+          { 'todoapp__clear-completed_none': !hasCompleteTodos },
+
+        )}
+        onClick={clearCompleted}
+      >
+        Clear completed
+      </button>
+    </footer>
+  ),
+);
