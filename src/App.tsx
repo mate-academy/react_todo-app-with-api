262e192c7b9e9559d0f4b9d2ba7ccd092ee69de7
Pavel Maistrenko <maystrenko.p@gmail.com>
2023-03-23 13:38:10 +0100

    adding missing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,170 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import { useState, useEffect } from 'react';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
+import { TodoList } from './components/TodoList/TodoList';
+import { ErrorNotification }
+  from './components/ErrorNotification/ErrorNotification';
 
-const USER_ID = 0;
+import { getTodos, addTodo, deleteTodo } from './api/todos';
+
+import { Todo } from './types/Todo';
+import { SortType } from './types/SortType';
+
+const USER_ID = 6650;
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
+  const [todosList, setTodosList] = useState<Todo[]>([]);
+  const [query, setQuery] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [errorType, setErrorType] = useState('');
+  const [sortType, setSortType] = useState(SortType.ALL);
+  const [isDisabledForm, setIsDisabledForm] = useState(false);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [tempTodos, setTempTodos] = useState<Todo[]>([]);
+
+  let visibleTodoList = todosList;
+  const copletedTodos = todosList.filter(todo => todo.completed);
+  const totalTodoListLength = todosList.length;
+  const activeTodoListLength = todosList.filter(
+    todo => !todo.completed,
+  ).length;
+  const isActiveFooter = !!totalTodoListLength;
+  const completedTodoListLength = totalTodoListLength - activeTodoListLength;
+
+
+  switch (sortType) {
+    case SortType.ACTIVE:
+      visibleTodoList = todosList.filter(todo => !todo.completed);
+      break;
+    case SortType.COMPLETE:
+      visibleTodoList = todosList.filter(todo => todo.completed);
+      break;
+    default:
+      break;
   }
 
-  return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">React Todo App - Add and Delete</a>
-      </p>
+  const showError = (value: string) => {
+    setIsError(true);
+    setErrorType(value);
+    window.setTimeout(() => {
+      setIsError(false);
+    }, 3000);
+  };
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+  const getTodosFromServer = async () => {
+    try {
+      const todosData = await getTodos(USER_ID);
+
+      setTodosList(todosData);
+    } catch {
+      setIsError(true);
+      showError('loading error');
+    }
+  };
+
+  const addTodoOnServer = async () => {
+    setIsDisabledForm(true);
+
+    try {
+      const todo = {
+        id: 0,
+        title: query,
+        userId: USER_ID,
+        completed: false,
+      };
+
+      setTempTodo(todo);
+
+      await addTodo(USER_ID, query);
+      await getTodosFromServer();
+
+      setIsDisabledForm(false);
+      setQuery('');
+      setTempTodo(null);
+    } catch {
+      setQuery('');
+      showError('unable to add element');
+    } finally {
+      setIsDisabledForm(false);
+    }
+  };
+
+  const deleteTodoFromServer = async (id:number) => {
+    try {
+      const selectedTodo = todosList.find(todo => todo.id === id);
+
+      if (selectedTodo) {
+        setTempTodos((prev) => [...prev, selectedTodo]);
+      }
+
+      await deleteTodo(id);
+      await getTodosFromServer();
+    } catch {
+      showError('unable to delete element');
+    } finally {
+      setTempTodos([]);
+    }
+  };
+
+  useEffect(() => {
+    getTodosFromServer();
+  }, []);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!query) {
+      return showError('Title can\'t be empty');
+    }
+
+    return addTodoOnServer();
+  };
+
+  const removeTodo = (id:number) => {
+    deleteTodoFromServer(id);
+  };
+
+  const removeCompletedTodos = () => {
+    copletedTodos.map(todo => deleteTodoFromServer(todo.id));
+  };
+
+  return (
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          totalTodoListLength={totalTodoListLength}
+          query={query}
+          onSetQuery={setQuery}
+          isDisabledForm={isDisabledForm}
+          handleSubmit={handleSubmit}
+        />
+
+        <TodoList
+          todos={visibleTodoList}
+          tempTodo={tempTodo}
+          tempTodos={tempTodos}
+          removeTodo={removeTodo}
+        />
+
+        {isActiveFooter && (
+          <Footer
+            sortType={sortType}
+            onSetSortType={setSortType}
+            activeTodoListLength={activeTodoListLength}
+            completedTodoListLength={completedTodoListLength}
+            removeCompletedTodos={removeCompletedTodos}
+          />
+        )}
+
+      </div>
+
+      <ErrorNotification
+        isError={isError}
+        setIsError={setIsError}
+        errorType={errorType}
+      />
+    </div>
   );
 };
