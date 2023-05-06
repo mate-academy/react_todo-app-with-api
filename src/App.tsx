65aaa39aebdfcd9bc9075699ed249a1fd47c144d
Olecsiy <infernoregent@gmail.com>
2023-05-06 17:28:08 +0300

    solution v4

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,25 @@ import {
   postTodo,
   updateTodo,
 } from './api/todos';
-
-const USER_ID = 6999;
+import { USER_ID, temporeryTodo } from './utils/constant';
+import { ErrorMessage } from './utils/errorMessage';
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [tempTodo, setTempTodo] = useState<Todo>({
-    id: 0,
-    title: '',
-    completed: false,
-    userId: 0,
-  });
+  const [tempTodo, setTempTodo] = useState<Todo>(temporeryTodo);
   const [filterBy, setFilterBy] = useState(StatusToFilterBy.All);
   const [isAdding, setIsAdding] = useState(false);
   const [isInputDisabled, setIsInputDisabled] = useState(false);
   const [loadingTodosId, setLoadingTodosId] = useState<Set<number>>(new Set());
   const inCompleteTodos = todos.filter(todo => todo.completed === false);
+
+  const handleErrorMessage = () => {
+    setTimeout(() => {
+      setError(ErrorMessage.Clear);
+    }, 3000);
+  };
 
   const handleTodosId = (todoId: number) => {
     setLoadingTodosId((state) => {
@@ -48,71 +49,62 @@ export const App: React.FC = () => {
 
   const getTodosFromServer = async () => {
     setIsLoading(true);
-    setError('');
+    setError(ErrorMessage.Clear);
 
     try {
       const todosFromServer = await getTodos(USER_ID);
 
       setTodos(todosFromServer);
     } catch {
-      setError('Server error!');
-      setTimeout(() => {
-        setError('');
-      }, 3000);
+      setError(ErrorMessage.Server);
+      handleErrorMessage();
     } finally {
       setIsLoading(false);
     }
   };
 
-  const handleAddTodo = (title: string) => {
+  const handleAddTodo = async (title: string): Promise<void> => {
     setIsAdding(true);
     if (!title.trim().length) {
-      setError("Title can't be empty");
-    } else {
-      const newTodo = {
-        id: 0,
-        userId: USER_ID,
-        completed: false,
-        title,
-      };
+      setError(ErrorMessage.Title);
 
-      setTempTodo(newTodo);
-      setError('');
-      postTodo(USER_ID, newTodo)
-        .then((todo: Todo) => {
-          handleTodosId(todo.id);
-          setTodos((prevTodos) => {
-            return [...prevTodos, todo];
-          });
-          removeTodosId(todo.id);
-        })
-        .catch(() => {
-          setError('Unable to add a todo!');
-          setTimeout(() => {
-            setError('');
-          }, 3000);
-        })
-        .finally(() => {
-          setIsAdding(false);
-        });
+      return;
+    }
+
+    const newTodo = {
+      id: 0,
+      userId: USER_ID,
+      completed: false,
+      title,
+    };
+
+    setTempTodo(newTodo);
+    setError(ErrorMessage.Clear);
+    try {
+      const todo = await postTodo(USER_ID, newTodo);
+
+      handleTodosId(todo.id);
+      setTodos((prevTodos) => [...prevTodos, todo]);
+      removeTodosId(todo.id);
+    } catch {
+      setError(ErrorMessage.AddTodo);
+      handleErrorMessage();
+    } finally {
+      setIsAdding(false);
     }
   };
 
-  const handleDeleteTodo = (id: number) => {
-    handleTodosId(id);
-    deleteTodo(id)
-      .then(() => {
-        setTodos(todos.filter(todo => todo.id !== id));
-      })
-      .catch(() => {
-        setError('Unable to delete a todo');
-        setTimeout(() => {
-          setError('');
-        }, 3000);
-      })
-      .finally(() => {
-        removeTodosId(id);
-      });
+  const handleDeleteTodo = async (id: number) => {
+    try {
+      handleTodosId(id);
+      await deleteTodo(id);
+      setTodos(todos.filter(todo => todo.id !== id));
+    } catch {
+      setError(ErrorMessage.DeleteTodo);
+      handleErrorMessage();
+    } finally {
+      removeTodosId(id);
+    }
   };
 
   const deleteCompleteTodos = useCallback(() => {
@@ -141,10 +133,8 @@ export const App: React.FC = () => {
         return prevTodo;
       }));
     } catch {
-      setError('Unable to update a todo!');
-      setTimeout(() => {
-        setError('');
-      }, 3000);
+      setError(ErrorMessage.UpdateTodo);
+      handleErrorMessage();
     } finally {
       removeTodosId(todoId);
       setIsInputDisabled(false);
@@ -239,7 +229,7 @@ export const App: React.FC = () => {
             type="button"
             className="delete"
             aria-label="Close Error button"
-            onClick={() => setError('')}
+            onClick={() => setError(ErrorMessage.Clear)}
           />
           {error}
         </div>
---
+++ src/utils/errorMessage.tsx
@@ -0,0 +1,8 @@
+export enum ErrorMessage {
+  Clear = '',
+  Title = "Title can't be empty",
+  Server = 'Server error!',
+  AddTodo = 'Unable to add a todo!',
+  DeleteTodo = 'Unable to delete a todo',
+  UpdateTodo = 'Unable to update a todo!',
+}
