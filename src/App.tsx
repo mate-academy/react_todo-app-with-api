53e086f3b65997c93ea4d4df7ba1bd2c78d9534f
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-05-17 11:27:16 +0300

    React Todo app with api v.0.1 - Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,200 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList/TodoList';
+import { PatchedTodo, Todo } from './types/Todo';
+import {
+  deleteTodo,
+  getTodos,
+  postTodo,
+  updateTodo,
+  changeTodos,
+} from './api/todos';
+import { Errors } from './types/Errors';
+import { FilterType } from './types/FilterType';
 
-const USER_ID = 0;
+const USER_ID = 10209;
+
+const todosFromServer = (todos: Todo[], filterType: string) => {
+  switch (filterType) {
+    case FilterType.ALL:
+      return todos;
+
+    case FilterType.ACTIVE:
+      return todos.filter((todo) => !todo.completed);
+
+    case FilterType.COMPLETED:
+      return todos.filter((todo) => todo.completed);
+
+    default:
+      return [];
+  }
+};
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState<Errors>(Errors.NONE);
+  const [filterType, setFilterType] = useState<FilterType>(FilterType.ALL);
+  const [title, setTitle] = useState('');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => setError(Errors.UPLOAD));
+  }, []);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
-  return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">React Todo App - Add and Delete</a>
-      </p>
+  const visibleTodos = todosFromServer(todos, filterType);
+  const completedTodos = todosFromServer(todos, FilterType.COMPLETED);
+  const isToggleOnActive = todos.length === completedTodos.length;
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+  const handleError = (e: Errors) => {
+    setError(e);
+    setTimeout(() => {
+      setError(Errors.NONE);
+    }, 3000);
+  };
+
+  const handleAddTodo = async (value: string) => {
+    if (!value) {
+      setError(Errors.EMPTY);
+      setTimeout(() => {
+        setError(Errors.NONE);
+      }, 3000);
+
+      return;
+    }
+
+    const newTodo = {
+      id: 0,
+      userId: USER_ID,
+      title,
+      completed: false,
+    };
+
+    setTempTodo(newTodo);
+
+    const postedTodo = await postTodo(USER_ID, newTodo);
+
+    setTodos((prev: Todo[]) => {
+      return [...prev, postedTodo];
+    });
+    setTempTodo(null);
+    setTitle('');
+  };
+
+  const handleDeleteTodo = (todoId: number) => {
+    deleteTodo(USER_ID, todoId)
+      .then(() => setTodos(todos.filter(todo => todo.id !== todoId)))
+      .catch(() => setError(Errors.DELETE));
+  };
+
+  const handleClearCompleted = () => {
+    const completedTodoIds = todos
+      .filter(todo => todo.completed)
+      .map(todo => todo.id);
+
+    Promise.all(completedTodoIds.map(id => deleteTodo(USER_ID, id)))
+      .then(() => setTodos(todos.filter(todo => !todo.completed)))
+      .catch(() => setError(Errors.DELETE));
+  };
+
+  const handleToggle = async (id: number, data: PatchedTodo) => {
+    try {
+      const updatedTodo = await updateTodo(id, data);
+
+      setTodos((prevTodos) => {
+        const index = prevTodos.findIndex((todo) => todo.id === id);
+
+        if (index === -1) {
+          return prevTodos;
+        }
+
+        const updatedTodos = [...prevTodos];
+
+        updatedTodos[index] = updatedTodo;
+
+        return updatedTodos;
+      });
+    } catch {
+      setError(Errors.UPDATE);
+    }
+  };
+
+  const handleToggleAll = async () => {
+    try {
+      const idsToToggle = todos
+        .filter(todo => (todo.completed === isToggleOnActive))
+        .map(todo => todo.id);
+
+      const toggledTodos = await Promise.all(
+        idsToToggle.map(id => updateTodo(id, { completed: !isToggleOnActive })),
+      );
+
+      setTodos(changeTodos(todos, toggledTodos));
+    } catch {
+      setError(Errors.UPDATE);
+    }
+  };
+
+  return (
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          todos={visibleTodos}
+          hasSomeTodos={!!todos.length}
+          onChangeIsError={handleError}
+          onSubmitAddTodo={handleAddTodo}
+          titleTodo={title}
+          onChangeTitle={setTitle}
+          onToggleAll={handleToggleAll}
+        />
+
+        <TodoList
+          tempTodo={tempTodo}
+          todos={visibleTodos}
+          onChangeIsError={handleError}
+          onDelete={handleDeleteTodo}
+          onChangeTodo={handleToggle}
+        />
+
+        {!!todos.length && (
+          <Footer
+            todos={visibleTodos}
+            filterType={filterType}
+            setFilterType={setFilterType}
+            handleClearCompleted={handleClearCompleted}
+          />
+        )}
+      </div>
+
+      {error
+        && (
+          <div
+            className={
+              classNames(
+                'notification is-danger is-light has-text-weight-normal',
+                { hidden: !error },
+              )
+            }
+          >
+            <button
+              type="button"
+              className="delete"
+              onClick={() => setError(Errors.NONE)}
+            />
+            {error}
+          </div>
+        )}
+    </div>
   );
 };
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,139 @@
+import classNames from 'classnames';
+import { useState } from 'react';
+import { PatchedTodo, Todo } from '../../types/Todo';
+import { Errors } from '../../types/Errors';
+import { updateTodoTitle } from '../../api/todos';
+
+  type Props = {
+    todo: Todo
+    onChangeIsError: (e: Errors) => void
+    onDeleteTodo: (todoId: number) => void
+    onChangeTodo:
+    (id: number, data: PatchedTodo) => void;
+  };
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  onChangeIsError,
+  onDeleteTodo,
+  onChangeTodo,
+}) => {
+  let { title } = todo;
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
+
+  const handleDeleteTodo = () => {
+    try {
+      onDeleteTodo(todo.id);
+    } catch {
+      onChangeIsError(Errors.DELETE);
+    }
+  };
+
+  const handleChangeTitle = () => {
+    setIsEditing(true);
+  };
+
+  const cancelEditing = () => {
+    setIsEditing(false);
+    setNewTitle(title);
+  };
+
+  const updateTitle = () => {
+    if (newTitle === title) {
+      cancelEditing();
+    }
+
+    if (newTitle.trim() === '') {
+      onDeleteTodo(todo.id);
+
+      return;
+    }
+
+    try {
+      updateTodoTitle(todo.id, newTitle);
+      title = newTitle;
+      setIsEditing(false);
+    } catch {
+      setIsEditing(true);
+      onChangeIsError(Errors.UPDATE);
+    } finally {
+      setIsEditing(false);
+      title = newTitle;
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    updateTitle();
+  };
+
+  const handleBlur = async () => {
+    setIsEditing(false);
+
+    if (newTitle !== title) {
+      updateTitle();
+    }
+  };
+
+  const handleEscape = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Escape') {
+      cancelEditing();
+    }
+  };
+
+  return (
+    <>
+      <section className="todoapp__main">
+        <div
+          className={classNames('todo', { completed: todo.completed })}
+        >
+          <label className="todo__status-label">
+            <input
+              type="checkbox"
+              className="todo__status"
+              // eslint-disable-next-line max-len
+              onChange={() => onChangeTodo(todo.id, { completed: !todo.completed })}
+            />
+          </label>
+
+          {isEditing ? (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="todo__title todo__input"
+                value={newTitle}
+                onChange={(event) => {
+                  setNewTitle(event.target.value);
+                }}
+                onBlur={handleBlur}
+                onKeyUp={handleEscape}
+                // eslint-disable-next-line jsx-a11y/no-autofocus
+                autoFocus
+              />
+            </form>
+          ) : (
+            <span className="todo__title" onDoubleClick={handleChangeTitle}>
+              {newTitle}
+            </span>
+          )}
+
+          <button
+            type="button"
+            className="todo__remove"
+            onClick={handleDeleteTodo}
+          >
+            ×
+          </button>
+
+          <div className="modal overlay">
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
