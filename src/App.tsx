aa82a455c995fdcdc28126c58bdf14656f167c98
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-05-22 16:25:36 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -101,9 +101,9 @@ export const App: FC = () => {
         .filter((todo) => todo.id !== todoToDelete.id));
     } catch {
       handleError(Error.DELETE);
-    } finally {
-      setIsLoading(false);
     }
+
+    setIsLoading(false);
   }, []);
 
   const handleDeleteCompletedTodos = useCallback(async () => {
