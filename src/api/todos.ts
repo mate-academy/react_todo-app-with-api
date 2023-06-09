243bf4427253745732b27e49a57a0d85dc708f28
Nikoramo <kanada387@gmail.com>
2023-06-09 21:29:33 +0200

    solution

---
+++ src/api/todos.ts
@@ -0,0 +1,20 @@
+import { Todo } from '../types';
+import { client } from '../utils/fetchClient';
+
+export const USER_ID = 10392;
+
+export const getTodos = (userId = 10392) => {
+  return client.get<Todo[]>(`/todos?userId=${userId}`);
+};
+
+export const createTodo = (userId: number, todo: Todo) => {
+  return client.post<Todo>(`/todos?userId=${userId}`, todo);
+};
+
+export const destroyTodo = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
+
+export const updateTodo = (id: number, todo: Todo) => {
+  return client.patch<Todo>(`/todos/${id}`, todo);
+};
