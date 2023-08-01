872463b3238c9945bf5ed2524da440f719b8eb8d
Illia Vinkov <illia.vinkov@gmail.com>
2023-08-01 07:58:41 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -16,24 +16,22 @@ enum Filter {
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [filterType, setFilterType] = useState<Filter>(Filter.ALL);
-  const [isShowError, setIsShowError] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
+
   const [query, setQuery] = useState<string>('');
   const [isTodoLoaded, setIsTodoLoaded] = useState<boolean>(false);
 
-  const [deleteTodoIds, setDeleteTodoIds] = useState([0]);
-  const [updatedTodoIds, setUpdatedTodoIds] = useState([0]);
+  const [processingTodoIds, setProcessingTodoIds] = useState<number[]>([0]);
 
   const [isTodoEditing, setIsTodoEditing] = useState<boolean>(false);
   const [todoEditing, setTodoEditing] = useState<Todo | null>(null);
   const [newTitle, setNewTitle] = useState<string>('');
 
   const handleErrorMessage = (message: string) => {
-    setIsShowError(true);
     setError(message);
 
     setTimeout(() => {
-      setIsShowError(false);
+      setError('');
     }, 3000);
   };
 
@@ -61,15 +59,14 @@ export const App: React.FC = () => {
     completed: completedTodos,
   };
 
-  const visibleTodos:Todo[] = filteredTodos[
-    filterType as keyof typeof filteredTodos
-  ];
+  const visibleTodos: Todo[]
+  = filteredTodos[filterType as keyof typeof filteredTodos];
 
   const handleFilterChange = (selectedFilter: Filter) => {
     setFilterType(selectedFilter);
   };
 
-  const handleErrorDelete = () => setIsShowError(false);
+  const handleErrorDelete = () => setError('');
 
   const addTodo = async (event: React.FormEvent, title: string) => {
     event.preventDefault();
@@ -105,7 +102,7 @@ export const App: React.FC = () => {
 
   const deleteTodo = async (currentId: number) => {
     try {
-      setDeleteTodoIds(prevIds => [...prevIds, currentId]);
+      setProcessingTodoIds(prevIds => [...prevIds, currentId]);
       await postService.deleteTodo(currentId);
       setTodos(prevTodos => prevTodos.filter(todo => todo.id !== currentId));
     } catch {
@@ -121,15 +118,13 @@ export const App: React.FC = () => {
 
   const updateTodo = async (updatedTodo: Todo) => {
     try {
-      setUpdatedTodoIds(prevIds => [...prevIds, updatedTodo.id]);
+      setProcessingTodoIds(prevIds => [...prevIds, updatedTodo.id]);
 
       await postService.updateTodo(updatedTodo);
 
       setTodos(prevTodos => {
         const newTodos = [...prevTodos];
-        const index = newTodos.findIndex(
-          todo => todo.id === updatedTodo.id,
-        );
+        const index = newTodos.findIndex(todo => todo.id === updatedTodo.id);
 
         newTodos.splice(index, 1, updatedTodo);
 
@@ -138,7 +133,7 @@ export const App: React.FC = () => {
     } catch {
       handleErrorMessage('Unable to update a todo');
     } finally {
-      setUpdatedTodoIds([0]);
+      setProcessingTodoIds([0]);
     }
   };
 
@@ -153,8 +148,8 @@ export const App: React.FC = () => {
     const allTodosCompleted = todos.every(todo => todo.completed);
 
     todos.forEach(todo => {
-      if ((allTodosCompleted && todo.completed)
-        || (!allTodosCompleted && !todo.completed)) {
+      if ((allTodosCompleted && todo.completed) || (
+        !allTodosCompleted && !todo.completed)) {
         checkTodo(todo, !todo.completed);
       }
     });
@@ -192,200 +187,173 @@ export const App: React.FC = () => {
   };
 
   return (
-    <div className="todoapp">
-      <h1 className="todoapp__title">todos</h1>
-
-      <div className="todoapp__content">
-        <header className="todoapp__header">
-          <button
-            type="button"
-            className={classNames('todoapp__toggle-all', {
-              active: activeTodos.length === 0,
-            })}
-            onClick={handleToggleAll}
-          />
-
-          <form onSubmit={(event) => addTodo(event, query)}>
-            <input
-              type="text"
+    <section className="section container">
+      <div className="todoapp">
+        <h1 className="todoapp__title">todos</h1>
+        <div className="todoapp__content">
+          <header className="todoapp__header">
+            <button
+              type="button"
               className={classNames(
-                'todoapp__new-todo',
-                { 'todoapp__new-todo__load': isTodoLoaded },
+                'todoapp__toggle-all', { active: activeTodos.length === 0 },
               )}
-              placeholder="What needs to be done?"
-              onChange={handleTodoInput}
-              value={query}
+              onClick={handleToggleAll}
             />
-          </form>
-        </header>
+            <form onSubmit={(event) => addTodo(event, query)}>
+              <input
+                type="text"
+                className={classNames('todoapp__new-todo',
 
-        {todos.length > 0 && (
-          <section className="todoapp__main">
-            {visibleTodos.map(todo => {
-              const { id, title, completed } = todo;
+                  { 'todoapp__new-todo__load': isTodoLoaded })}
+                placeholder="What needs to be done?"
+                onChange={handleTodoInput}
+                value={query}
+              />
+            </form>
+          </header>
 
-              return (
-                <div
-                  className={classNames('todo', { completed })}
-                  key={id}
-                >
-                  <label className="todo__status-label">
-                    <input
-                      type="checkbox"
-                      className="todo__status"
-                      checked={completed}
-                      onClick={() => checkTodo(todo)}
-                    />
-                  </label>
+          {todos.length > 0 && (
+            <section className="todoapp__main">
+              {visibleTodos.map(todo => {
+                const { id, title, completed } = todo;
 
-                  {(isTodoEditing && todoEditing?.id === id) ? (
-                    <form onSubmit={
-                      (event) => changeTodoTitle(event, newTitle)
-                    }
-                    >
+                return (
+                  <div className={classNames('todo', { completed })} key={id}>
+                    <label className="todo__status-label">
                       <input
-                        type="text"
-                        className="todo__title-field"
-                        placeholder="Will be deleted if empty"
-                        onChange={handleTodoUpdated}
-                        value={newTitle}
+                        type="checkbox"
+                        className="todo__status"
+                        checked={completed}
+                        onClick={() => checkTodo(todo)}
                       />
-                    </form>
-                  ) : (
-                    <>
-                      <span
-                        className="todo__title"
-                        onDoubleClick={() => handleDoubleClick(todo)}
-                      >
-                        {title}
-                      </span>
+                    </label>
 
-                      <button
-                        type="button"
-                        className="todo__remove"
-                        onClick={() => deleteTodo(id)}
+                    {isTodoEditing && todoEditing?.id === id ? (
+                      <form onSubmit={(
+                        event,
+                      ) => changeTodoTitle(event, newTitle)}
                       >
-                        ×
-                      </button>
-                    </>
-                  )}
+                        <input
+                          type="text"
+                          className="todo__title-field"
+                          placeholder="Will be deleted if empty"
+                          onChange={handleTodoUpdated}
+                          value={newTitle}
+                        />
+                      </form>
+                    ) : (
+                      <>
+                        <span
+                          className="todo__title"
+                          onDoubleClick={() => handleDoubleClick(todo)}
+                        >
+                          {title}
+                        </span>
 
-                  <div className={classNames(
-                    'modal',
-                    'overlay',
-                    {
-                      'is-active':
-                        deleteTodoIds.includes(id)
-                        || updatedTodoIds.includes(id),
-                    },
-                  )}
-                  >
-                    <div className="
-                      modal-background
-                      has-background-white-ter
-                      "
+                        <button
+                          type="button"
+                          className="todo__remove"
+                          onClick={() => deleteTodo(id)}
+                        >
+                          ×
+                        </button>
+                      </>
+                    )}
+
+                    <div
+                      className={classNames('modal', 'overlay', {
+                        'is-active': processingTodoIds.includes(id),
+                      })}
+                    >
+                      <div
+                        className="modal-background has-background-white-ter"
+                      />
+                      <div className="loader" />
+                    </div>
+                  </div>
+                );
+              })}
+
+              {query && isTodoLoaded && (
+                <div className="todo">
+                  <label className="todo__status-label">
+                    <input type="checkbox" className="todo__status" />
+                  </label>
+                  <span className="todo__title">Todo is being saved now</span>
+                  <div className="modal overlay is-active">
+                    <div
+                      className="modal-background has-background-white-ter"
                     />
                     <div className="loader" />
                   </div>
                 </div>
-              );
-            })}
-
-            {query && isTodoLoaded && (
-              <div className="todo">
-                <label className="todo__status-label">
-                  <input type="checkbox" className="todo__status" />
-                </label>
-
-                <span className="todo__title">
-                  Todo is being saved now
-                </span>
-
-                <div className="modal overlay is-active">
-                  <div className="
-                    modal-background
-                    has-background-white-ter
-                    "
-                  />
-                  <div className="loader" />
-                </div>
-              </div>
-            )}
-          </section>
-        )}
-
-        {todos.length > 0 && (
-          <footer className="todoapp__footer">
-            <span className="todo-count">
-              {`${activeTodos.length} ${activeTodos.length > 1 ? 'items' : 'item'} left`}
-            </span>
-
-            <nav className="filter">
-              <a
-                role="button"
-                href="#/"
-                className={classNames(
-                  'filter__link',
-                  'filter__link__all',
-                  { selected: filterType === Filter.ALL },
-                )}
-                onClick={() => handleFilterChange(Filter.ALL)}
-              >
-                All
-              </a>
-
-              <a
-                href="#/active"
-                className={classNames(
-                  'filter__link',
-                  'filter__link__active',
-                  { selected: filterType === Filter.ACTIVE },
-                )}
-                onClick={() => handleFilterChange(Filter.ACTIVE)}
-              >
-                Active
-              </a>
-
-              <a
-                href="#/completed"
-                className={classNames(
-                  'filter__link',
-                  'filter__link__completed',
-                  { selected: filterType === Filter.COMPLETED },
-                )}
-                onClick={() => handleFilterChange(Filter.COMPLETED)}
-              >
-                Completed
-              </a>
-            </nav>
-
-            <button
-              type="button"
-              className={classNames(
-                'todoapp__clear-completed',
-                'clear-completed',
-                { 'clear-completed__hide': completedTodos.length === 0 },
               )}
-              onClick={handleClearCompleted}
-            >
-              Clear completed
-            </button>
-          </footer>
-        )}
-      </div>
+            </section>
+          )}
 
-      <div
-        className="
-          notification
-          is-danger
-          is-light
-          has-text-weight-normal
-        "
-        hidden={!isShowError}
-      >
-        <button type="button" className="delete" onClick={handleErrorDelete} />
-        {error}
+          {todos.length > 0 && (
+            <footer className="todoapp__footer">
+              <span className="todo-count">
+                {`${activeTodos.length} ${activeTodos.length > 1 ? 'items' : 'item'} left`}
+              </span>
+              <nav className="filter">
+                <a
+                  role="button"
+                  href="#/"
+                  className={classNames('filter__link',
+                    'filter__link__all', {
+                      selected:
+                      filterType === Filter.ALL,
+                    })}
+                  onClick={() => handleFilterChange(Filter.ALL)}
+                >
+                  All
+                </a>
+                <a
+                  href="#/active"
+                  className={classNames('filter__link', 'filter__link__active',
+                    { selected: filterType === Filter.ACTIVE })}
+                  onClick={() => handleFilterChange(Filter.ACTIVE)}
+                >
+                  Active
+                </a>
+                <a
+                  href="#/completed"
+                  className={classNames('filter__link',
+                    'filter__link__completed',
+                    { selected: filterType === Filter.COMPLETED })}
+                  onClick={() => handleFilterChange(Filter.COMPLETED)}
+                >
+                  Completed
+                </a>
+              </nav>
+              <button
+                type="button"
+                className={classNames('todoapp__clear-completed',
+                  'clear-completed', {
+                    'clear-completed__hide': completedTodos.length
+                  === 0,
+                  })}
+                onClick={handleClearCompleted}
+              >
+                Clear completed
+              </button>
+            </footer>
+          )}
+        </div>
+
+        <div
+          className="notification is-danger is-light has-text-weight-normal"
+          hidden={!error}
+        >
+          <button
+            type="button"
+            className="delete"
+            onClick={handleErrorDelete}
+          />
+          {error}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
