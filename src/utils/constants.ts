29d1840b72477e70efa291ba88a090073eaf9051
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-08-07 15:55:55 +0300

    todos app

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+/* eslint-disable max-len */
+export const FETCH_ERROR = 'Failed to fetch todos from the server. Please check your internet connection and try again later.';
+export const ADDING_ERROR = 'Unable to add a todo';
+export const DELETING_ERROR = 'Unable to delete a todo';
+export const TITLE_ERROR = "Title can't be empty";
+export const UPDATING_ERROR = 'Unable to update a todo';
+
+export const USER_ID = 10344;
+
+export const ERROR_TIMEOUT = 3000;
