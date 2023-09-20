7312d0d47fac9049dc8f5843222856bc1b0a0b8c
Mykhailo Shevchuk <mishashevchuk@gmail.com>
2023-09-20 21:24:38 +0300

    todo app completed

---
+++ src/types/Error.ts
@@ -0,0 +1,9 @@
+export enum Error {
+  None = '',
+  Load = 'Unable to load todos',
+  Add = 'Unable to add todo',
+  Delete = 'Unable to delete a todo',
+  Update = 'Unable to update a todo',
+  EmptyTitle = 'Title can\'t be empty',
+  Toggle = 'Unable to toggle todos',
+}
