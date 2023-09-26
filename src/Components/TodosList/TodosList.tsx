7e67faa4b54eaee89b34db0b77c648ecdd2d4934
Andrii Marusiak <advo737@gmail.com>
2023-09-26 11:10:46 +0300

    add Transitiongroup

--- src/Components/TodosList/TodosList.tsx
+++ src/Components/TodosList/TodosList.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
+import classNames from 'classnames';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-import { useTodosContext } from '../../hooks/getContextHooks';
+import { useTodosContext, useApiErrorContext }
+  from '../../hooks/getContextHooks';
 import { TodoItem } from '../TodoItem';
 
 import getFilteredTodos from '../../helpers/getFilteredTodos';
 
 export const TodosList: React.FC = () => {
   const { todos, filter, tempTodo } = useTodosContext();
+  const { apiError } = useApiErrorContext();
 
   const filteredTodos = getFilteredTodos(todos, filter);
 
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {filteredTodos.map(todo => <TodoItem todo={todo} key={todo.id} />)}
-      {tempTodo && (
-        <TodoItem todo={tempTodo} key={tempTodo.id} />
-      )}
+      <TransitionGroup>
+        {filteredTodos.map(todo => (
+          <CSSTransition
+            key={todo.id}
+            timeout={300}
+            classNames="item"
+          >
+            <TodoItem todo={todo} />
+          </CSSTransition>
+        ))}
+
+        {tempTodo && (
+          <CSSTransition
+            key={0}
+            timeout={300}
+            classNames={classNames('temp-item', {
+              'has-error': apiError,
+            })}
+          >
+            <TodoItem todo={tempTodo} key={tempTodo.id} />
+          </CSSTransition>
+
+        )}
+      </TransitionGroup>
     </section>
   );
 };
