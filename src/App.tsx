9e7aed57d0b0d4c16f210336a8705f737a3389d9
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-09-26 16:45:31 +0300

    imlement work with todos Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,254 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
+import React, { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
 
-const USER_ID = 0;
+import { Todo } from './types/Todo';
+import * as todoService from './api/todos';
+import { TodoRow } from './components/TodoRow';
+import { TodoHeader } from './components/TodoHeader';
+import { Status } from './types/Status';
+import { TodoFilter } from './components/TodoFilter';
+
+const filterTodos = (todos: Todo[], isCompleted: Status): Todo[] => {
+  return todos.filter((todo: Todo) => {
+    switch (isCompleted) {
+      case Status.Completed:
+        return todo.completed;
+      case Status.Active:
+        return !todo.completed;
+      default:
+        return true;
+    }
+  });
+};
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [processingTodoIds, setProcessingTodoIds] = useState<number[]>([]);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [status, setStatus] = useState<Status>(Status.All);
+
+  useEffect(() => {
+    todoService
+      .getTodos()
+      .then(setTodos)
+      .catch(() => {
+        setErrorMessage('Unable to load todos');
+      });
+  }, []);
+
+  const timerId = useRef<number>(0);
+
+  useEffect(() => {
+    if (timerId.current) {
+      window.clearTimeout(timerId.current);
+    }
+
+    timerId.current = window.setTimeout(() => {
+      setErrorMessage('');
+    }, 3000);
+  }, [errorMessage]);
+
+  const handleAddTodo = (todoTitle: string) => {
+    setTempTodo({
+      id: 0,
+      title: todoTitle,
+      userId: 0,
+      completed: false,
+    });
+
+    return todoService
+      .addTodos(todoTitle)
+      .then((newTodo) => {
+        setTodos((prevTodos) => [...prevTodos, newTodo]);
+      })
+      .catch(() => {
+        setErrorMessage('Unable to add a todo');
+        throw new Error();
+      })
+      .finally(() => {
+        setTempTodo(null);
+      });
+  };
+
+  const handleDeleteTodo = (todoId: number) => {
+    setProcessingTodoIds(prevTodoIds => [...prevTodoIds, todoId]);
+
+    todoService
+      .deleteTodos(todoId)
+      .then((() => {
+        setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== todoId));
+      }))
+      .catch(() => {
+        setErrorMessage('Unable to delete a todo');
+      })
+      .finally(() => {
+        setProcessingTodoIds(prevTodoIds => prevTodoIds
+          .filter(id => id !== todoId));
+      });
+  };
+
+  const handleRenameTodo = (todo: Todo, newTodoTitle: string) => {
+    setProcessingTodoIds(prevTodoids => [...prevTodoids, todo.id]);
+
+    todoService
+      .updateTodo({
+        id: todo.id,
+        title: newTodoTitle,
+        userId: todo.userId,
+        completed: todo.completed,
+      })
+      .then(updatedTodo => {
+        setTodos(prevState => prevState.map(currentTodo => {
+          return currentTodo.id !== updatedTodo.id ? currentTodo : updatedTodo;
+        }));
+      })
+      .catch(() => {
+        setErrorMessage('Unable to update a todo');
+      })
+      .finally(() => {
+        setProcessingTodoIds(prevTodoIds => prevTodoIds
+          .filter(id => id !== todo.id));
+      });
+  };
+
+  const handleChangeStatusTodo = (todo: Todo) => {
+    setProcessingTodoIds(prevTodoids => [...prevTodoids, todo.id]);
+
+    todoService
+      .updateTodo({
+        id: todo.id,
+        title: todo.title,
+        userId: todo.userId,
+        completed: !todo.completed,
+      })
+      .then(updatedTodo => {
+        setTodos(prevState => prevState.map(currentTodo => {
+          return currentTodo.id !== updatedTodo.id ? currentTodo : updatedTodo;
+        }));
+      })
+      .catch(() => {
+        setErrorMessage('Unable to update a todo');
+      })
+      .finally(() => {
+        setProcessingTodoIds(prevTodoIds => prevTodoIds
+          .filter(id => id !== todo.id));
+      });
+  };
+
+  const handleClearCompletedTodos = () => {
+    todos
+      .filter(todo => todo.completed)
+      .forEach(todo => {
+        handleDeleteTodo(todo.id);
+      });
+  };
+
+  const visibleTodos = filterTodos(todos, status);
+  const handleFilterStatus = (todosFilter: Status) => (
+    setStatus(todosFilter));
+
+  const allTodosCount = todos.length;
+  const CompletedTodosCount = todos
+    .filter(todo => todo.completed === true).length;
+  const isAllTodosCompleted = allTodosCount === CompletedTodosCount;
+
+  const handleChangeStatusTodos = () => {
+    if (isAllTodosCompleted) {
+      todos
+        .forEach(todo => {
+          handleChangeStatusTodo(todo);
+        });
+    } else {
+      todos
+        .filter(todo => todo.completed === false)
+        .forEach(todo => {
+          handleChangeStatusTodo(todo);
+        });
+    }
+  };
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">React Todo App - Add and Delete</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <TodoHeader
+          onTodoAdd={handleAddTodo}
+          onTodoAddError={setErrorMessage}
+          isAllTodosCompleted={isAllTodosCompleted}
+          onTodosChangeStatus={handleChangeStatusTodos}
+          todos={visibleTodos}
+          isTodosHere={Boolean(allTodosCount)}
+        />
+
+        <section className="todoapp__main" data-cy="TodoList">
+          {visibleTodos.map(todo => {
+            return (
+              <TodoRow
+                todo={todo}
+                key={todo.id}
+                onTodoDelete={() => handleDeleteTodo(todo.id)}
+                onTodoRename={(todoTitle) => handleRenameTodo(todo, todoTitle)}
+                onTodoChangeStatus={() => handleChangeStatusTodo(todo)}
+                isProcessing={processingTodoIds.includes(todo.id)}
+              />
+            );
+          })}
+
+          {tempTodo && (
+            <TodoRow
+              todo={tempTodo}
+              isProcessing
+            />
+          )}
+        </section>
+        {Boolean(todos.length) && (
+          <footer className="todoapp__footer" data-cy="Footer">
+            <span className="todo-count" data-cy="TodosCounter">
+              {`${todos.length - CompletedTodosCount} items left`}
+            </span>
+
+            <TodoFilter
+              handleFilterStatus={handleFilterStatus}
+              todosFilterStatus={status}
+            />
+
+            <button
+              type="button"
+              className="todoapp__clear-completed"
+              disabled={!CompletedTodosCount}
+              data-cy="ClearCompletedButton"
+              onClick={handleClearCompletedTodos}
+            >
+              Clear completed
+            </button>
+          </footer>
+        )}
+      </div>
+
+      <div
+        data-cy="ErrorNotification"
+        className={classNames(
+          'notification',
+          'is-danger',
+          'is-light',
+          'has-text-weight-normal', {
+            hidden: !errorMessage,
+          },
+        )}
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={() => {
+            setErrorMessage('');
+          }}
+        />
+        {errorMessage}
+      </div>
+    </div>
   );
 };
---
+++ src/components/TodoHeader.tsx
@@ -0,0 +1,86 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import classNames from 'classnames';
+import React, { useEffect, useRef, useState } from 'react';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  onTodoAdd: (todoTitle: string) => Promise<void>;
+  onTodoAddError: (errorMessage: string) => void;
+  isAllTodosCompleted: boolean;
+  onTodosChangeStatus: () => void;
+  todos: Todo[];
+  isTodosHere: boolean;
+};
+
+export const TodoHeader: React.FC<Props> = ({
+  onTodoAdd,
+  onTodoAddError,
+  isAllTodosCompleted,
+  onTodosChangeStatus,
+  todos,
+  isTodosHere,
+}) => {
+  const [todoTitle, setTodoTitle] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+
+  const onTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoTitle(event.target.value);
+  };
+
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const trimedTodoTitle = todoTitle.trim();
+
+    if (!trimedTodoTitle) {
+      onTodoAddError('Title should not be empty');
+
+      return;
+    }
+
+    setIsAdding(true);
+    onTodoAdd(trimedTodoTitle)
+      .then(() => {
+        setTodoTitle('');
+      })
+      .finally(() => {
+        setIsAdding(false);
+      });
+  };
+
+  const titleField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (titleField.current) {
+      titleField.current.focus();
+    }
+  }, [todos.length]);
+
+  return (
+    <header className="todoapp__header">
+      {isTodosHere && (
+        <button
+          data-cy="ToggleAllButton"
+          onClick={onTodosChangeStatus}
+          type="button"
+          className={classNames('todoapp__toggle-all', {
+            active: isAllTodosCompleted,
+          })}
+        />
+      )}
+
+      <form onSubmit={onFormSubmit}>
+        <input
+          ref={titleField}
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={todoTitle}
+          onChange={onTitleChange}
+          disabled={isAdding}
+        />
+      </form>
+    </header>
+  );
+};
