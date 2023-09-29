34276b20093a30d3c307e06e7a370d1db155e66c
Orest Kozak <orestkozak17@gmail.com>
2023-09-29 18:59:18 +0300

    todo app with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,332 @@
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, {
+  useCallback,
+  useEffect, useRef, useState,
+} from 'react';
+
+import classNames from 'classnames';
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList/TodoList';
+import * as todoService from './api/todos';
+import { Status } from './types/Status';
+import { USER_ID } from './api/userId';
 import { UserWarning } from './UserWarning';
 
-const USER_ID = 0;
-
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [status, setStatus] = useState<Status>(Status.All);
+  const [visible, setVisible] = useState(false);
+  const [disableInput, setDisableInput] = useState(false);
+  const [tempoTodo, setTempoTodo] = useState<Todo | null>(null);
+  const [loadingTodoId, setLoadingTodoId] = useState<number[]>([]);
+
+  const completedTodosCount = todos.reduce((acc, todo) => {
+    return todo.completed ? acc + 1 : acc;
+  }, 0);
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const hasCompleted = todos.every(todo => todo.completed);
+
+  const activeTodosCount = todos.length - completedTodosCount;
+
+  const visibleTodos = todos.filter(todo => {
+    switch (status) {
+      case Status.Active:
+        return !todo.completed;
+
+      case Status.Completed:
+        return todo.completed;
+
+      case Status.All:
+      default:
+        return true;
+    }
+  });
+
+  const resetAddTodoState = () => {
+    setTempoTodo(null);
+    setTitle('');
+    setDisableInput(false);
+  };
+
+  const createTempTodo = () => ({
+    id: 0,
+    userId: USER_ID,
+    title: title.trim(),
+    completed: false,
+  });
+
+  const handleSubmit = (todoTitle: string) => {
+    const trimmedTitle = todoTitle.trim();
+    const tempTodo = createTempTodo();
+
+    setTempoTodo(tempTodo);
+
+    setDisableInput(true);
+
+    if (!trimmedTitle) {
+      setErrorMessage("Title can't be empty");
+
+      return;
+    }
+
+    setTimeout(() => {
+      todoService
+        .addTodos(tempTodo)
+        .then((newTodo) => setTodos([...todos, newTodo]))
+        .catch(() => setErrorMessage('Unable to add a todo'))
+        .finally(() => resetAddTodoState());
+    }, 500);
+  };
+
+  const deleteTodo = (todoId: number) => {
+    setLoadingTodoId([todoId]);
+
+    todoService.deletePost(todoId)
+      .then(() => setTodos([
+        ...todos.filter(todo => todo.id !== todoId),
+      ]))
+      .catch(() => setErrorMessage('Unable to delete the todo'));
+  };
+
+  const handleClearCompletedTodos = useCallback(() => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    const completedTodoIds = completedTodos.map(todo => todo.id);
+
+    setLoadingTodoId(completedTodoIds);
+
+    Promise.all(
+      completedTodos.map(todo => (
+        todoService.deletePost(todo.id)
+      )),
+    )
+      .then(() => {
+        setTodos(todos.filter(todo => !todo.completed));
+      })
+      .catch(() => {
+        setErrorMessage('Unable to delete completed todos');
+      })
+      .finally(() => setLoadingTodoId([]));
+  }, [todos]);
+
+  const toggleTodoStatus = async (todoId: number, completed: boolean) => {
+    try {
+      setLoadingTodoId([todoId]);
+      await todoService.updatePostStatus(todoId, completed);
+
+      setTodos(currentTodos => currentTodos.map(
+        todo => (todoId === todo.id ? { ...todo, completed } : todo),
+      ));
+    } catch {
+      setErrorMessage('Unable to update a todo');
+    } finally {
+      setLoadingTodoId([]);
+    }
+  };
+
+  const handleToggleTodosAll = async (completed: boolean) => {
+    const todosIds = todos
+      .filter(todo => todo.completed !== completed)
+      .map(todo => todo.id);
+
+    try {
+      setLoadingTodoId(todosIds);
+
+      const updatedTodos = todos.map(todo => ({
+        ...todo,
+        completed,
+      }));
+
+      setTodos(updatedTodos);
+
+      await Promise.all(
+        todos.map(todo => todoService.updatePostStatus(todo.id, completed)),
+      );
+    } catch {
+      setErrorMessage('Unable to update a todos');
+    } finally {
+      setLoadingTodoId([]);
+    }
+  };
+
+  const handleEditTodo = async (todoId: number, newTitle: string) => {
+    if (!newTitle) {
+      setErrorMessage('Title can`t be empty');
+
+      return;
+    }
+
+    try {
+      setLoadingTodoId([todoId]);
+      const updatedTodo = await todoService.updateTodoTitle(todoId, newTitle);
+
+      setTodos(currentTodos => currentTodos.map(
+        todo => (todoId === todo.id ? updatedTodo : todo),
+      ));
+    } catch {
+      setErrorMessage('Title can`t be empty');
+    } finally {
+      setLoadingTodoId([]);
+    }
+  };
+
+  useEffect(() => {
+    if (!disableInput && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [disableInput]);
+
+  useEffect(() => {
+    if (errorMessage) {
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+    }
+  }, [errorMessage]);
+
+  useEffect(() => {
+    todoService.getTodos()
+      .then(setTodos)
+      .catch(() => setErrorMessage('Unable to load a todo'));
+
+    setTimeout(() => {
+      setVisible(true);
+    }, 1000);
+  }, []);
+
   if (!USER_ID) {
-    return <UserWarning />;
+    return (
+      <UserWarning />
+    );
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">React Todo App - Add and Delete</a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div
+        className={classNames('todoapp__content', {
+          visible,
+        })}
+      >
+        <header className="todoapp__header">
+          {todos.length > 0 && (
+            <button
+              type="button"
+              className={`todoapp__toggle-all ${hasCompleted ? 'active' : ''} `}
+              onClick={() => handleToggleTodosAll(!hasCompleted)}
+            />
+          )}
+
+          <form onSubmit={(e) => {
+            handleSubmit(title);
+            e.preventDefault();
+          }}
+          >
+            <input
+              type="text"
+              className="todoapp__new-todo"
+              placeholder="What needs to be done?"
+              value={title}
+              onChange={event => setTitle(event.target.value)}
+              ref={inputRef}
+              disabled={disableInput}
+            />
+          </form>
+        </header>
+
+        {(todos.length > 0 || tempoTodo) && (
+          <div
+            className={classNames('todo-list', {
+              visible,
+            })}
+          >
+
+            <TodoList
+              todos={visibleTodos}
+              deleteTodo={deleteTodo}
+              tempoTodo={tempoTodo}
+              uptadeTodoStatus={toggleTodoStatus}
+              loadingTodoId={loadingTodoId}
+              onChangeTodoTitle={handleEditTodo}
+            />
+
+            <footer className="todoapp__footer">
+              <span className="todo-count">
+                {`${activeTodosCount} items left`}
+              </span>
+
+              <nav className="filter">
+                <a
+                  href="#/"
+                  className={classNames('filter__link', {
+                    selected: status === Status.All,
+                  })}
+                  onClick={() => setStatus(Status.All)}
+
+                >
+                  All
+                </a>
+
+                <a
+                  href="#/active"
+                  className={classNames('filter__link', {
+                    selected: status === Status.Active,
+                  })}
+                  onClick={() => setStatus(Status.Active)}
+                >
+                  Active
+                </a>
+
+                <a
+                  href="#/completed"
+                  className={classNames('filter__link', {
+                    selected: status === Status.Completed,
+                  })}
+                  onClick={() => setStatus(Status.Completed)}
+                >
+                  Completed
+                </a>
+              </nav>
+
+              <button
+                type="button"
+                className={classNames('todoapp__clear-completed', {
+                  'todoapp__clear-completed--hidden': completedTodosCount === 0,
+                })}
+                onClick={handleClearCompletedTodos}
+              >
+                Clear completed
+              </button>
+            </footer>
+          </div>
+        )}
+      </div>
+
+      <div
+        className={classNames(
+          'notification',
+          'is-danger',
+          'is-light',
+          'has-text-weight-normal',
+          {
+            hidden: !errorMessage,
+          },
+        )}
+      >
+        <button
+          type="button"
+          className="delete"
+          onClick={() => setErrorMessage('')}
+        />
+
+        {errorMessage}
+      </div>
+    </div>
   );
 };
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo,
+  deleteTodo: (todoId: number) => void,
+  uptadeTodoStatus: (todoId: number, completed: boolean) => void,
+  onChangeTodoTitle: (todoId: number, newTitle: string) => void,
+  loading: boolean,
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo, loading,
+  deleteTodo, uptadeTodoStatus, onChangeTodoTitle,
+}) => {
+  const { id, completed, title } = todo;
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+
+  const titleField = useRef<HTMLInputElement>(null);
+
+  const handleDeleteTodo = () => {
+    deleteTodo(id);
+  };
+
+  const titleChanger = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedTitle(event.target.value);
+  };
+
+  const handleTodoStatusChange = () => {
+    uptadeTodoStatus(id, !completed);
+  };
+
+  const handleEditTitle = () => {
+    setIsEditing(true);
+
+    if (!editedTitle) {
+      handleDeleteTodo();
+    } else if (editedTitle === title) {
+      setIsEditing(false);
+    }
+
+    onChangeTodoTitle(id, editedTitle);
+    setIsEditing(false);
+  };
+
+  const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleEditTitle();
+    } else if (event.key === 'Escape') {
+      setEditedTitle(title);
+      setIsEditing(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isEditing) {
+      titleField.current?.focus();
+    }
+  }, [isEditing, id]);
+
+  return (
+    <>
+      <div className={classNames(
+        'todo',
+        { completed },
+      )}
+      >
+        <label className="todo__status-label">
+          <input
+            type="checkbox"
+            className="todo__status"
+            checked={completed}
+            onClick={handleTodoStatusChange}
+            id={`toggle-completed-${id}`}
+            disabled={loading}
+            ref={titleField}
+          />
+        </label>
+
+        {!isEditing ? (
+          <>
+            <span
+              className="todo__title"
+              onDoubleClick={() => setIsEditing(true)}
+            >
+              {todo.title}
+            </span>
+
+            <button
+              type="button"
+              className="todo__remove"
+              onClick={handleDeleteTodo}
+              disabled={loading}
+            >
+              ×
+            </button>
+          </>
+        ) : (
+          <input
+            type="text"
+            className="todo__title"
+            value={editedTitle}
+            ref={titleField}
+            onKeyUp={handleKeyUp}
+            onBlur={handleEditTitle}
+            onChange={titleChanger}
+          />
+        )}
+
+        <div className={classNames('modal overlay', {
+          'is-active': loading,
+        })}
+        >
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+    </>
+  );
+};
