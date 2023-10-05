7ceed8af5575073b77769d9063eac1804e5ae1de
neovendano <kolesnyk.vik@gmail.com>
2023-10-05 18:37:53 +0300

    fix error timer

--- src/components/ErrorNotification/ErrorNotification.tsx
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classNames from 'classnames';
 import { ErrorMessage } from '../../types/ErrorMessage';
 
@@ -11,6 +11,22 @@ export const ErrorNotification: React.FC<Props> = ({
   errorMessage,
   setErrorMessage,
 }) => {
+  const timerRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    if (errorMessage) {
+      timerRef.current = window.setTimeout(() => {
+        setErrorMessage(ErrorMessage.Default);
+      }, 3000);
+    }
+
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, [errorMessage]);
+
   return (
     <div
       className={classNames(
