c215afa201a15887d83e56dbcd439ec03b834941
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-10-09 11:37:52 +0300

    fixed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -38,17 +38,17 @@ export const Footer: React.FC<Props> = (
 
       {/* Active filter should have a 'selected' class */}
       <nav className="filter" data-cy="Filter">
-        {/*{Object.values(SortType).map((sortType) => (*/}
-        {/*  <a*/}
-        {/*    onClick={() => changeSelectFilter(sortType)}*/}
-        {/*    href="#/"*/}
-        {/*    className={cn('filter__link',*/}
-        {/*      { selected: selectFilter === sortType })}*/}
-        {/*    data-cy="FilterLinkAll"*/}
-        {/*  >*/}
-        {/*    {sortType}*/}
-        {/*  </a>*/}
-        {/*))}*/}
+        {/* {Object.values(SortType).map((sortType) => ( */}
+        {/*  <a */}
+        {/*    onClick={() => changeSelectFilter(sortType)} */}
+        {/*    href="#/" */}
+        {/*    className={cn('filter__link', */}
+        {/*      { selected: selectFilter === sortType })} */}
+        {/*    data-cy="FilterLinkAll" */}
+        {/*  > */}
+        {/*    {sortType} */}
+        {/*  </a> */}
+        {/* ))} */}
 
         <a
           onClick={() => changeSelectFilter(SortType.All)}
