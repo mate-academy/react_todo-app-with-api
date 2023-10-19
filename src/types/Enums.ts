2a10286eb3538609ec014e223632640365b8c42c
Edgar Kocuba <edgarkocuba@gmail.com>
2023-10-19 14:43:35 +0300

    solution 1

---
+++ src/types/Enums.ts
@@ -0,0 +1,13 @@
+export enum ErrMessage {
+  loadTodo = 'Unable to load todos',
+  titleEmpty = 'Title should not be empty',
+  addTodo = 'Unable to add a todo',
+  deleteTodo = 'Unable to delete a todo',
+  updateTodo = 'Unable to update a todo',
+}
+
+export enum FilterBy {
+  all = 'all',
+  active = 'active',
+  completed = 'completed',
+}
