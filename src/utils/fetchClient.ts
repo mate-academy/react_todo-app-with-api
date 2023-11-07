2e5c88e22d2a350210a1034344fd5a1ca8ecdc10
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-11-07 11:15:47 +0100

    rewrote request function in fetchClient to async/await

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -11,7 +11,7 @@ function wait(delay: number) {
 // To have autocompletion and avoid mistypes
 type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
-function request<T>(
+async function request<T>(
   url: string,
   method: RequestMethod = 'GET',
   data: any = null, // we can send any data to the server
@@ -27,15 +27,15 @@ function request<T>(
   }
 
   // DON'T change the delay it is required for tests
-  return wait(100)
-    .then(() => fetch(BASE_URL + url, options))
-    .then(response => {
-      if (!response.ok) {
-        throw new Error();
-      }
+  await wait(100);
 
-      return response.json();
-    });
+  const response = await fetch(BASE_URL + url, options);
+
+  if (!response.ok) {
+    throw new Error();
+  }
+
+  return response.json();
 }
 
 export const client = {
