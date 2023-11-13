1f52476313de4adce0e2ae317c661232502820a7
elenagrasovskaya <elenagrasovskaya@gmail.com>
2023-11-13 11:57:17 +0300

    animation added

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,50 +1,29 @@
-import cn from 'classnames';
 import { useContext } from 'react';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 import { TodoItem } from '../TodoItem';
 import { TodosContext } from '../TodosProvider';
 
 export const TodoList: React.FC = () => {
-  const {
-    filteredTodos,
-    tempTodo,
-  } = useContext(TodosContext);
+  const { filteredTodos, tempTodo } = useContext(TodosContext);
 
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {filteredTodos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id}/>
-      ))}
-      {tempTodo && (
-        <>
-          <div data-cy="Todo" className="todo">
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-              />
-            </label>
+      <TransitionGroup>
+        {filteredTodos.map((todo) => (
+          <CSSTransition key={todo.id} timeout={300} classNames="item">
+            <TodoItem todo={todo} />
+          </CSSTransition>
+        ))}
 
-            <span data-cy="TodoTitle" className="todo__title">
-              {tempTodo.title}
-            </span>
-
-            <button type="button" className="todo__remove" data-cy="TodoDelete">
-              ×
-            </button>
-
-            <div
-              data-cy="TodoLoader"
-              className={cn('modal overlay', {
-                'is-active': tempTodo,
-              })}
-            >
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div>
-        </>
-      )}
+        {tempTodo && (
+          <CSSTransition key={0} timeout={300} classNames="temp-item">
+            <TodoItem todo={tempTodo} />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </section>
   );
 };
