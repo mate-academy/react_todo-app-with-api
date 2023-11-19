c1ad5002538fd340c944b600967300803ce9112b
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-11-19 13:09:13 +0200

    added new logic in TodoFilter

--- src/TodoFilter.tsx
+++ src/TodoFilter.tsx
@@ -9,33 +9,17 @@ export const TodoFilter: React.FC<TodoFilterProps> = ({
 }) => {
   return (
     <nav className="filter" data-cy="Filter">
-      <a
-        href="#/"
-        onClick={handleFilterClick(FilterBy.All)}
-        className={cn('filter__link',
-          { selected: filterBy === FilterBy.All })}
-        data-cy="FilterLinkAll"
-      >
-        All
-      </a>
-      <a
-        href="#/active"
-        onClick={handleFilterClick(FilterBy.Active)}
-        className={cn('filter__link',
-          { selected: filterBy === FilterBy.Active })}
-        data-cy="FilterLinkActive"
-      >
-        Active
-      </a>
-      <a
-        href="#/completed"
-        onClick={handleFilterClick(FilterBy.Completed)}
-        className={cn('filter__link',
-          { selected: filterBy === FilterBy.Completed })}
-        data-cy="FilterLinkCompleted"
-      >
-        Completed
-      </a>
+      {Object.values(FilterBy).map(filterType => (
+        <a
+          key={filterType}
+          href={`#/${filterType === FilterBy.All && filterType}`}
+          onClick={handleFilterClick(filterType)}
+          className={cn('filter__link', { selected: filterBy === filterType })}
+          data-cy={`FilterLink${filterType}`}
+        >
+          {filterType}
+        </a>
+      ))}
     </nav>
   );
 };
