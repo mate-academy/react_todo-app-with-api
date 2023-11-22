ef112e1f6923d85e83332c9df4ddc90449913fac
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-11-22 18:40:20 +0200

    implement solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { TodoProvider } from './components/TodoContext';
+import { USER_ID } from './utils/UserId';
+import { TodoHeader } from './components/TodoHeader';
+import { TodoFooter } from './components/TodoFooter';
+import { TodoList } from './components/TodoList';
+import { ErrorNotification } from './components/ErrorNotification';
 
 export const App: React.FC = () => {
+  // const { todos } = useContext(TodoContext);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">React Todo App - Add and Delete</a>
-      </p>
-
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+    <TodoProvider>
+      <div className="todoapp">
+        <h1 className="todoapp__title">todos</h1>
+        <div className="todoapp__content">
+          <TodoHeader />
+          <TodoList />
+          <TodoFooter />
+        </div>
+        <ErrorNotification />
+      </div>
+    </TodoProvider>
   );
 };
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,44 @@
+import { useContext, useMemo } from 'react';
+import { TodoItem } from './TodoItem';
+import { TodoContext } from './TodoContext';
+import { Status } from '../types/Status';
+
+export const TodoList: React.FC = () => {
+  const {
+    todos, tempTodo, filter, todoIdsWithLoader,
+  } = useContext(TodoContext);
+
+  const filteredTodos = useMemo(() => {
+    switch (filter) {
+      case Status.Active:
+        return todos.filter(todo => !todo.completed);
+      case Status.Completed:
+        return todos.filter(todo => todo.completed);
+      case Status.All:
+      default:
+        return todos;
+    }
+  }, [filter, todos]);
+
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+
+      {filteredTodos.map(todo => (
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          isLoading={todoIdsWithLoader.includes(todo.id)}
+        />
+      ))}
+
+      { tempTodo && (
+        <TodoItem
+          todo={tempTodo}
+          key={tempTodo.id}
+          isLoading={todoIdsWithLoader.includes(tempTodo.id)}
+        />
+      )}
+
+    </section>
+  );
+};
