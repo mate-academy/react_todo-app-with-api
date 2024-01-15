fd8d1f872e110ae890144c3fad4f850fa74d3af8
Laura Wronska <laura.wronskaa@gmail.com>
2024-01-15 10:14:24 +0100

    move todo logic from TodoList to TodoInfo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,120 +1,17 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import classNames from 'classnames';
 import { useTodo } from '../providers/TodoProvider';
 import { TodoInfo } from './TodoInfo';
-import { TodoEdit } from './TodoEdit';
-import { Todo } from '../types/Todo';
 
 export const TodoList = () => {
   const {
     visibleTodos,
-    updateTodo,
-    modifiedTodo,
-    setModifiedTodo,
-    deleteTodoFromApi,
     tempTodo,
-    isDeleting,
-    isUpdating,
   } = useTodo();
-
-  const handleDoubleClick = (todoId: number | null = null) => () => {
-    setModifiedTodo(todoId);
-  };
-
-  const handleCheck = (todo: Todo) => () => {
-    updateTodo(todo.id, { completed: !todo.completed });
-  };
-
-  const handleDelete = (todoId: number) => () => {
-    deleteTodoFromApi(todoId);
-  };
 
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {visibleTodos.map(todo => {
-        return (
-          <div
-            key={todo.id}
-            data-cy="Todo"
-            className={classNames('todo', {
-              completed: todo.completed,
-            })}
-            onDoubleClick={handleDoubleClick(todo.id)}
-          >
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-                checked={todo.completed}
-                onChange={handleCheck(todo)}
-              />
-            </label>
+      {visibleTodos.map(todo => <TodoInfo todo={todo} />)}
 
-            {todo.id !== modifiedTodo
-              ? <TodoInfo todo={todo} />
-              : <TodoEdit todo={todo} />}
-            {todo.id !== modifiedTodo && (
-              <button
-                type="button"
-                className="todo__remove"
-                data-cy="TodoDelete"
-                onClick={handleDelete(todo.id)}
-              >
-                ×
-              </button>
-            ) }
-
-            <div
-              data-cy="TodoLoader"
-              className={classNames('modal overlay', {
-                'is-active': isDeleting.includes(todo.id)
-                  || isUpdating.includes(todo.id),
-              })}
-            >
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
-          </div>
-        );
-      })}
-
-      {tempTodo && (
-        <div
-          key={tempTodo.id}
-          data-cy="Todo"
-          className="todo"
-          onDoubleClick={handleDoubleClick(tempTodo.id)}
-        >
-          <label className="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              className="todo__status"
-              checked={tempTodo.completed}
-              onChange={handleCheck(tempTodo)}
-            />
-          </label>
-
-          {tempTodo.id !== modifiedTodo
-            ? <TodoInfo todo={tempTodo} />
-            : <TodoEdit todo={tempTodo} />}
-
-          <button
-            type="button"
-            className="todo__remove"
-            data-cy="TodoDelete"
-            onClick={handleDelete(tempTodo.id)}
-          >
-            ×
-          </button>
-          <div data-cy="TodoLoader" className="modal overlay is-active">
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
-          </div>
-        </div>
-      )}
+      {tempTodo && <TodoInfo todo={tempTodo} />}
     </section>
   );
 };
