a8a930a4be0915fc4c24362523cd965a8f123496
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-02-07 22:05:08 +0200

    done, but must check some tests

---
+++ src/types/Errors.ts
@@ -0,0 +1,7 @@
+export enum Errors {
+  LoadError = 'Unable to load todos',
+  EmptyTitle = 'Title should not be empty',
+  AddingError = 'Unable to add a todo',
+  DeletingError = 'Unable to delete a todo',
+  UpdatingError = 'Unable to update a todo',
+}
