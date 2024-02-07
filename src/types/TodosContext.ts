193c4a59e0fa0243af90d2879b3dceb5d642c00a
Vlad Pashko <vladpavloggg@gmail.com>
2024-02-07 19:34:36 +0200

    todo app with api

---
+++ src/types/TodosContext.ts
@@ -0,0 +1,17 @@
+import { Status } from './Status';
+import { Todo } from './Todo';
+
+export type TodoContext = {
+  todos: Todo[]
+  setTodos: (v: Todo[] | ((n: Todo[]) => Todo[])) => void
+  title: string
+  setTitle: React.Dispatch<React.SetStateAction<string>>
+  filterBy: Status;
+  setFilterBy: (filterBy: Status) => void
+  errorMessage: string
+  setErrorMessage: React.Dispatch<React.SetStateAction<string>>
+  tempTodo: Todo | null
+  setTempTodo: React.Dispatch<React.SetStateAction<Todo | null>>
+  editedTodos: Todo[]
+  setEditedTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+};
