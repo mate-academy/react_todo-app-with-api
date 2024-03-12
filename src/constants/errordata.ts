6032446d3c25b648103933621b5761aefd4cf1be
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-03-12 20:31:16 +0200

    last part of task

---
+++ src/constants/errordata.ts
@@ -0,0 +1,5 @@
+export const UNABLE_LOAD_ERROR = 'Unable to load todos';
+export const UNABLE_TO_ADD_ERROR = 'Unable to add a todo';
+export const UNABLE_TO_UPDATE_ERROR = 'Unable to update a todo';
+export const UNABLE_TO_DELETE_ERROR = 'Unable to delete a todo';
+export const EMPTY_TITLE_ERROR = 'Title should not be empty';
---
+++ src/constants/userdata.ts
@@ -0,0 +1,2 @@
+export const USER_ID = 238;
+export const BASE_URL = 'https://mate.academy/students-api';
