8cdb2576f552e51d887057e5527b845b84bb8080
Olena Shapkun <shapkunkh@gmail.com>
2024-03-13 11:40:51 +0200

    temp todo correction

--- src/components/TempTodo/TempTodo.tsx
+++ src/components/TempTodo/TempTodo.tsx
@@ -4,33 +4,33 @@ import { useTodos } from '../../Store';
 export const TempTodo: React.FC = () => {
   const { tempTodo, handleChangeStatus } = useTodos();
 
+  if (!tempTodo) {
+    return null;
+  }
+
   return (
-    <>
-      {tempTodo && (
-        <div data-cy="Todo" className="todo">
-          <div data-cy="TodoLoader" className="modal overlay is-active">
-            <div className="modal-background has-background-white-ter" />
-            <div className="loader" />
-          </div>
+    <div data-cy="Todo" className="todo">
+      <div data-cy="TodoLoader" className="modal overlay is-active">
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
 
-          <label className="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              className="todo__status"
-              checked={tempTodo.completed}
-              onChange={() => handleChangeStatus(tempTodo.id)}
-            />
-          </label>
-          <span data-cy="TodoTitle" className="todo__title">
-            {tempTodo.title}
-          </span>
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={tempTodo.completed}
+          onChange={() => handleChangeStatus(tempTodo.id)}
+        />
+      </label>
+      <span data-cy="TodoTitle" className="todo__title">
+        {tempTodo.title}
+      </span>
 
-          <button type="button" className="todo__remove" data-cy="TodoDelete">
-            ×
-          </button>
-        </div>
-      )}
-    </>
+      <button type="button" className="todo__remove" data-cy="TodoDelete">
+        ×
+      </button>
+    </div>
   );
 };
