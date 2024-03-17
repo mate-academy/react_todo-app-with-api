a2b9b291f2742f8b6bc0006278a5ecbaa44c22dd
Tetiana Koval <tetiana.koval.bit@gmail.com>
2024-03-17 19:57:48 +0200

    solution 8

---
+++ src/components/ErrorComponent/ErrorComponent.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import classNames from 'classnames';
+import { TodoContext } from '../../contexts/TodoContext';
+
+export const ErrorComponent: React.FC = () => {
+  const { dataError, setError } = useContext(TodoContext);
+
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification',
+        'is-danger',
+        'is-light',
+        'has-text-weight-normal',
+        { hidden: !dataError },
+      )}
+    >
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={() => setError('')}
+      >
+        {dataError}
+      </button>
+    </div>
+  );
+};
