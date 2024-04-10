b162a949fb91c57f1b3413f01794ee2519ecd37a
dmitruz <dmitruz970@gmail.com>
2024-04-10 21:21:00 +0300

    all tests passed

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,7 +1,7 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 404;
+export const USER_ID = 397;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
