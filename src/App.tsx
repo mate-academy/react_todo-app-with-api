3f99354af46922e676ab26d7cd673006619d8741
legogoUA <olehmedushivskyi@gmail.com>
2024-04-16 18:09:14 +0300

    fix code after review

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export const App: React.FC = () => {
       <div className="todoapp__content">
         <Header />
 
-        {todos.length !== 0 && (
+        {!!todos.length && (
           <>
             <TodoList />
             <Footer />
