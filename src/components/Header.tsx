1bfe6c6e74abdeb87db2c09e5fdab11c82c3b6e9
Erik Hulinian <gulinianerik2@gmail.com>
2024-05-09 22:39:25 +0300

    add solution

---
+++ src/components/Header.tsx
@@ -0,0 +1,95 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+
+import classNames from 'classnames';
+import { TodoContext } from '../services/TodoContext';
+
+type Props = {};
+
+export const Header: React.FC<Props> = () => {
+  const { todos, updateTodo, addTodo, setErrorMessage } =
+    useContext(TodoContext);
+  const [title, setTitle] = useState('');
+  const titleField = useRef<HTMLInputElement>(null);
+  const [creating, setCreating] = useState(false);
+  const [titleOnError, setTitleOnError] = useState('');
+  const completedTodos = todos.filter(todo => todo.completed);
+  const activeTodos = todos.filter(todo => !todo.completed);
+
+  useEffect(() => {
+    if (!creating) {
+      titleField.current?.focus();
+    }
+  }, [creating]);
+
+  const handleToggleTodos = () => {
+    if (completedTodos.length === todos.length) {
+      for (const todo of completedTodos) {
+        updateTodo({ ...todo, completed: false });
+      }
+
+      return;
+    }
+
+    for (const todo of activeTodos) {
+      updateTodo({ ...todo, completed: true });
+    }
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setErrorMessage('');
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (title.trim() === '') {
+      setErrorMessage('Title should not be empty');
+
+      return;
+    }
+
+    setTitleOnError(title);
+    setTitle('');
+    setCreating(true);
+    addTodo(title)
+      .then(() => {
+        setTitleOnError('');
+      })
+      .catch(() => {
+        setTitleOnError(title);
+      })
+      .finally(() => {
+        setCreating(false);
+        setTitle(titleOnError);
+      });
+  };
+
+  return (
+    <header className="todoapp__header">
+      {/* this button should have `active` class only if all todos are completed */}
+      <button
+        type="button"
+        className={classNames('todoapp__toggle-all', {
+          active: completedTodos.length === todos.length,
+        })}
+        data-cy="ToggleAllButton"
+        onClick={handleToggleTodos}
+      />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+          value={title}
+          onChange={handleTitleChange}
+          disabled={creating}
+          ref={titleField}
+        />
+      </form>
+    </header>
+  );
+};
---
+++ src/components/TodoInfo.tsx
@@ -0,0 +1,145 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import classNames from 'classnames';
+import { Todo } from '../types/Todo';
+import { TodoContext } from '../services/TodoContext';
+
+type Props = {
+  todo: Todo;
+  isLoading?: boolean;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo, isLoading = false }) => {
+  const { updateTodo, deleteTodo } = useContext(TodoContext);
+  const [beingEdited, setBeingEdited] = useState(false);
+  const [loading, setLoading] = useState(isLoading);
+  const [title, setTitle] = useState(todo.title);
+
+  const updateTodoTitle = () => {
+    setLoading(true);
+    setBeingEdited(false);
+
+    if (title.trim() === '') {
+      deleteTodo(todo.id);
+
+      return;
+    }
+
+    updateTodo({ ...todo, title: title }).finally(() => {
+      setLoading(false);
+      setBeingEdited(false);
+    });
+  };
+
+  const handleDoubleClick = () => {
+    setBeingEdited(true);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    updateTodoTitle();
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleChangeCompleted = () => {
+    updateTodo({ ...todo, completed: !todo.completed });
+  };
+
+  const handleDelete = () => {
+    setLoading(true);
+
+    deleteTodo(todo.id).finally(() => {
+      setLoading(false);
+    });
+  };
+
+  const handleKeyup = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setBeingEdited(false);
+      setTitle(todo.title);
+    } else if (event.key === 'Enter') {
+      updateTodoTitle();
+    }
+  };
+
+  useEffect(() => {
+    if (beingEdited) {
+      document.addEventListener('keyup', handleKeyup);
+
+      return () => {
+        document.removeEventListener('keyup', handleKeyup);
+      };
+    }
+
+    return;
+  });
+
+  return (
+    <>
+      {/* This is a completed todo */}
+      <div
+        data-cy="Todo"
+        className={classNames('todo', { completed: todo.completed })}
+      >
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label className="todo__status-label">
+          <input
+            data-cy="TodoStatus"
+            type="checkbox"
+            className="todo__status"
+            checked={todo.completed}
+            onChange={handleChangeCompleted}
+          />
+        </label>
+
+        {!beingEdited ? (
+          <>
+            <span
+              data-cy="TodoTitle"
+              className="todo__title"
+              onDoubleClick={handleDoubleClick}
+            >
+              {todo.title}
+            </span>
+
+            {/* Remove button appears only on hover */}
+            <button
+              type="button"
+              className="todo__remove"
+              data-cy="TodoDelete"
+              onClick={handleDelete}
+            >
+              ×
+            </button>
+          </>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <input
+              data-cy="TodoTitleField"
+              type="text"
+              className="todo__title-field"
+              placeholder="Empty todo will be deleted"
+              autoFocus
+              value={title}
+              onChange={handleTitleChange}
+              onBlur={updateTodoTitle}
+            />
+          </form>
+        )}
+
+        {/* overlay will cover the todo while it is being deleted or updated */}
+        <div
+          data-cy="TodoLoader"
+          className={classNames('modal overlay', { 'is-active': loading })}
+        >
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      </div>
+    </>
+  );
+};
