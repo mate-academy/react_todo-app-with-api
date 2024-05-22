8a08a420c000ab4a7d0eda5433ea9e147a2125d7
Oleksii Dunchyk <oleksiidunchik@gmail.com>
2024-05-22 20:47:23 +0300

    implemented todo renaming

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -2,6 +2,7 @@
 import { Todo } from '../../types/Todo';
 import { deleteTodo, updateTodo } from '../../api/todos';
 import { ErrorType } from '../../types/Error';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
   todo: Todo;
@@ -20,20 +21,30 @@ export const TodoItem: React.FC<Props> = ({
   setError,
   setLoadingIds,
 }) => {
-  const handleDeleteClick = (todoId: number) => {
-    setLoadingIds(prevIds => [...prevIds, todoId]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [editedTodo, setEditedTodo] = useState<Todo | null>(null);
+  const [editedTitle, setEditedTitle] = useState('');
 
-    deleteTodo(todoId)
+  useEffect(() => {
+    if (editedTodo) {
+      inputRef.current?.focus();
+    }
+  }, [editedTodo]);
+
+  const handleDelete = () => {
+    setLoadingIds(prevIds => [...prevIds, todo.id]);
+
+    deleteTodo(todo.id)
       .then(() =>
         setTodos((prevTodos: Todo[]) =>
-          prevTodos.filter(todoItem => todoItem.id !== todoId),
+          prevTodos.filter(todoItem => todoItem.id !== todo.id),
         ),
       )
       .catch(() => {
         setError(ErrorType.DeleteFail);
       })
       .finally(() => {
-        setLoadingIds(prevIds => prevIds.filter(id => id !== todoId));
+        setLoadingIds(prevIds => prevIds.filter(id => id !== todo.id));
       });
   };
 
@@ -55,6 +66,57 @@ export const TodoItem: React.FC<Props> = ({
       });
   };
 
+  const handleDoubleClick = () => {
+    setEditedTodo(todo);
+    setEditedTitle(todo.title);
+  };
+
+  const updateTodoTitle = () => {
+    const normTitle = editedTitle.trim();
+
+    if (normTitle.length === 0) {
+      handleDelete();
+
+      return;
+    }
+
+    if (normTitle === todo.title) {
+      setEditedTodo(null);
+
+      return;
+    }
+
+    setLoadingIds(prevIds => [...prevIds, todo.id]);
+
+    const newTodo = { ...todo, title: normTitle };
+
+    updateTodo(newTodo)
+      .then(() => {
+        setTodos((prevTodos: Todo[]) =>
+          prevTodos.map(todoItem =>
+            todoItem.id === todo.id ? newTodo : todoItem,
+          ),
+        );
+      })
+      .catch(() => setError(ErrorType.UpdateFail))
+      .finally(() => {
+        setLoadingIds(prevIds => prevIds.filter(id => id !== todo.id));
+      });
+  };
+
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    updateTodoTitle();
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Escape') {
+      setEditedTodo(null);
+      setEditedTitle(todo.title);
+    }
+  };
+
   return (
     <div
       data-cy="Todo"
@@ -71,17 +133,39 @@ export const TodoItem: React.FC<Props> = ({
         />
       </label>
 
-      <span data-cy="TodoTitle" className="todo__title">
-        {todo.title}
-      </span>
-      <button
-        type="button"
-        className="todo__remove"
-        data-cy="TodoDelete"
-        onClick={() => handleDeleteClick(todo.id)}
-      >
-        ×
-      </button>
+      {editedTodo === todo ? (
+        <form onSubmit={handleFormSubmit}>
+          <input
+            ref={inputRef}
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            value={editedTitle}
+            onChange={event => setEditedTitle(event.target.value)}
+            onKeyDown={handleKeyDown}
+            onBlur={updateTodoTitle}
+          />
+        </form>
+      ) : (
+        <>
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={handleDoubleClick}
+          >
+            {todo.title}
+          </span>
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={handleDelete}
+          >
+            ×
+          </button>
+        </>
+      )}
 
       <div
         data-cy="TodoLoader"
