13e1c59490782e425a2aa283cf8ff2203091b30f
Oleh Dynys <dynys.oleh@gmail.com>
2024-05-27 09:32:57 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { USER_ID, getTodos } from './api/todos';
+import { Todo } from './types/Todo';
+import { Status } from './types/Status';
+import { Header } from './components/Header/Header';
+import { getVisibleTodos } from './utils/getVisibleTodos';
+import { TodoList } from './components/TodoList/TodoList';
+import { Footer } from './components/Footer/Footer';
+import { TodoItem } from './components/TodoItem/TodoItem';
+import { ErrorNotification } from './components/Error/Error';
+import { ErrorMessages } from './types/ErrorMessages';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [selectedStatus, setSelectedStatus] = useState(Status.All);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [loadingId, setLoadingId] = useState<number[]>([]);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => setErrorMessage(ErrorMessages.UnableLoadTodos))
+      .finally(() => {
+        setTimeout(() => {
+          setErrorMessage('');
+        }, 3000);
+      });
+  }, []);
+
+  const filteredTodos = getVisibleTodos(todos, selectedStatus);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">
-          React Todo App - Add and Delete
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          loadingIds={loadingId}
+          setTodos={setTodos}
+          setTempTodo={setTempTodo}
+          setErrorMessage={setErrorMessage}
+          setLoadingIds={setLoadingId}
+        />
+        <TodoList
+          todos={filteredTodos}
+          loadingIds={loadingId}
+          setTodos={setTodos}
+          setErrorMessage={setErrorMessage}
+          setLoadingIds={setLoadingId}
+        />
+        {tempTodo && (
+          <TodoItem
+            todo={tempTodo}
+            isTemp={true}
+            loadingIds={loadingId}
+            setTodos={setTodos}
+            setErrorMessage={setErrorMessage}
+            setLoadingIds={setLoadingId}
+          />
+        )}
+        {todos.length > 0 && (
+          <Footer
+            todos={todos}
+            selectedStatus={selectedStatus}
+            setSelectedStatus={setSelectedStatus}
+            setTodos={setTodos}
+            setErrorMessage={setErrorMessage}
+            setLoadingIds={setLoadingId}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      />
+    </div>
   );
 };
---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,179 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useEffect, useRef, useState } from 'react';
+import { Todo } from '../../types/Todo';
+import { deleteTodos, updateTodo } from '../../api/todos';
+import { ErrorMessages } from '../../types/ErrorMessages';
+
+interface Props {
+  todo: Todo;
+  isTemp: boolean;
+  loadingIds: number[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setErrorMessage: (message: string) => void;
+  setLoadingIds: React.Dispatch<React.SetStateAction<number[]>>;
+}
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  isTemp,
+  loadingIds,
+  setTodos,
+  setErrorMessage,
+  setLoadingIds,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [editingTodo, setEditingTodo] = useState<Todo | null>(null);
+  const [editTitle, setEditTitle] = useState('');
+
+  useEffect(() => {
+    if (editingTodo) {
+      inputRef.current?.focus();
+    }
+  }, [editingTodo]);
+
+  const handleDeleteClick = () => {
+    setLoadingIds(prevIds => [...prevIds, todo.id]);
+
+    deleteTodos(todo.id)
+      .then(() =>
+        setTodos((prevTodos: Todo[]) =>
+          prevTodos.filter(todoItem => todoItem.id !== todo.id),
+        ),
+      )
+      .catch(() => {
+        setErrorMessage(ErrorMessages.UnableDelete);
+      })
+      .finally(() => {
+        setLoadingIds(prevIds => prevIds.filter(id => id !== todo.id));
+      });
+  };
+
+  const handleCheck = () => {
+    setLoadingIds(prevIds => [...prevIds, todo.id]);
+    const updatedTodo = { ...todo, completed: !todo.completed };
+
+    updateTodo(updatedTodo)
+      .then(() => {
+        setTodos((prevTodos: Todo[]) =>
+          prevTodos.map(todoItem =>
+            todoItem.id === todo.id ? updatedTodo : todoItem,
+          ),
+        );
+      })
+      .catch(() => setErrorMessage(ErrorMessages.UnableUpdate))
+      .finally(() => {
+        setLoadingIds(prevIds => prevIds.filter(id => id !== todo.id));
+      });
+  };
+
+  const handleDoubleClick = () => {
+    setEditingTodo(todo);
+    setEditTitle(todo.title);
+  };
+
+  const updateTodoTitle = () => {
+    const normTitle = editTitle.trim();
+
+    if (normTitle.length === 0) {
+      handleDeleteClick();
+
+      return;
+    }
+
+    if (normTitle === todo.title) {
+      setEditingTodo(null);
+
+      return;
+    }
+
+    setLoadingIds(prevIds => [...prevIds, todo.id]);
+
+    const newTodo = { ...todo, title: normTitle };
+
+    updateTodo(newTodo)
+      .then(() => {
+        setTodos((prevTodos: Todo[]) =>
+          prevTodos.map(todoItem =>
+            todoItem.id === todo.id ? newTodo : todoItem,
+          ),
+        );
+      })
+      .catch(() => setErrorMessage(ErrorMessages.UnableUpdate))
+      .finally(() => {
+        setLoadingIds(prevIds => prevIds.filter(id => id !== todo.id));
+      });
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    updateTodoTitle();
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Escape') {
+      setEditingTodo(null);
+      setEditTitle(todo.title);
+    }
+  };
+
+  return (
+    <div
+      data-cy="Todo"
+      className={`todo ${todo.completed && 'completed'}`}
+      key={todo.id}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          onClick={handleCheck}
+        />
+      </label>
+
+      {editingTodo === todo ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            ref={inputRef}
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            value={editTitle}
+            onChange={event => setEditTitle(event.target.value)}
+            onKeyDown={handleKeyPress}
+            onBlur={updateTodoTitle}
+          />
+        </form>
+      ) : (
+        <>
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={handleDoubleClick}
+          >
+            {todo.title}
+          </span>
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={handleDeleteClick}
+          >
+            ×
+          </button>
+        </>
+      )}
+
+      <div
+        data-cy="TodoLoader"
+        className={`modal overlay ${(isTemp || loadingIds.includes(todo.id)) && 'is-active'}`}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
