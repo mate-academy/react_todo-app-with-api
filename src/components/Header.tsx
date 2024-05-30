cf42a6723a54a9bc07371ca309c269b47e08011c
valost <valerieostrovska@yahoo.com>
2024-05-30 15:40:48 +0300

    add task solution

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import React, { FormEvent } from 'react';
+import { Todo } from '../types/Todo';
+import cn from 'classnames';
+
+type Props = {
+  todos: Todo[];
+  inputRef: React.RefObject<HTMLInputElement>;
+  handleAddingTodo: (event: FormEvent<HTMLFormElement>) => void;
+  title: string;
+  handleTypingTodo: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleTogglingAllTodos: () => void;
+};
+
+export const Header: React.FC<Props> = ({
+  todos,
+  inputRef,
+  handleAddingTodo,
+  title,
+  handleTypingTodo,
+  handleTogglingAllTodos,
+}) => {
+  const activeTodos = todos.filter(todo => !todo.completed);
+
+  return (
+    <header className="todoapp__header">
+      {todos.length !== 0 && (
+        <button
+          type="button"
+          className={cn('todoapp__toggle-all', {
+            active: activeTodos.length === 0,
+          })}
+          data-cy="ToggleAllButton"
+          onClick={handleTogglingAllTodos}
+        />
+      )}
+
+      <form onSubmit={handleAddingTodo}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          ref={inputRef}
+          value={title}
+          onChange={handleTypingTodo}
+        />
+      </form>
+    </header>
+  );
+};
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,121 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useRef, useState } from 'react';
+import { Todo } from '../types/Todo';
+import cn from 'classnames';
+
+type Props = {
+  todo: Todo;
+  handleDeletingTodo?: () => void;
+  isLoading: boolean;
+  handleUpdatingTodo?: (todo: Todo) => void;
+  handleTogglingTodo?: () => void;
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  handleDeletingTodo = () => {},
+  isLoading,
+  handleUpdatingTodo = () => {},
+  handleTogglingTodo = () => {},
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(todo.title);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmittingUpdate = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const preparedTitle = newTitle.trim();
+
+    setNewTitle(preparedTitle);
+
+    if (preparedTitle === todo.title) {
+      setIsEditing(false);
+
+      return;
+    }
+
+    if (!preparedTitle) {
+      handleDeletingTodo();
+
+      return;
+    }
+
+    handleUpdatingTodo({ ...todo, title: preparedTitle });
+  };
+
+  return (
+    <div
+      data-cy="Todo"
+      className={cn('todo', {
+        completed: todo.completed,
+      })}
+      key={todo.id}
+    >
+      <label className="todo__status-label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          onChange={handleTogglingTodo}
+        />
+      </label>
+
+      {isEditing ? (
+        <form
+          onSubmit={handleSubmittingUpdate}
+          onKeyUp={event => {
+            if (event.key === 'Escape') {
+              setIsEditing(false);
+            }
+          }}
+          onBlur={handleSubmittingUpdate}
+        >
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            value={newTitle}
+            ref={inputRef}
+            onChange={event => setNewTitle(event.target.value)}
+          />
+        </form>
+      ) : (
+        <>
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={() => {
+              setIsEditing(true);
+              setNewTitle(todo.title);
+            }}
+          >
+            {todo.title}
+          </span>
+
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={handleDeletingTodo}
+          >
+            ×
+          </button>
+        </>
+      )}
+
+      <div
+        data-cy="TodoLoader"
+        className={cn('modal overlay', {
+          'is-active': isLoading,
+        })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
