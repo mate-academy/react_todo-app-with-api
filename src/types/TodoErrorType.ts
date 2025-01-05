44d55651255b566784d5c68f3f4c57c14e425397
Svitlana <svitlana.gaiduk@gmail.com>
2025-01-05 23:47:12 +0200

    add task solution

---
+++ src/types/TodoErrorType.ts
@@ -0,0 +1,8 @@
+export enum TodoErrorType {
+  UnableToLoadTodos = 'Unable to load todos',
+  UnableToAddTodo = 'Unable to add a todo',
+  UnableToDeleteTodo = 'Unable to delete a todo',
+  UnableToUpdateTodo = 'Unable to update a todo',
+  EmptyTitle = 'Title should not be empty',
+  None = '',
+}
