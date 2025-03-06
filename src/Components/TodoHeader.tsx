a4807f7c7806cbd21bf59e7b4ab1ea012a7d7527
Oleksii Petrychenko <apetr@MacBook-Air.local>
2025-03-06 23:38:48 +0200

    add task solution

---
+++ src/Components/TodoHeader.tsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+
+type Props = {
+  todoTitleRef: React.RefObject<HTMLInputElement> | null;
+  onAddTodo: (title: string) => Promise<boolean>;
+  onToggleAllTodos: () => void;
+  isLoading: boolean;
+  isAllTodoCompleted: boolean;
+  isTodoListNotEmpty: boolean;
+};
+
+export const TodoHeader: React.FC<Props> = ({
+  todoTitleRef,
+  onAddTodo,
+  onToggleAllTodos,
+  isLoading,
+  isAllTodoCompleted,
+  isTodoListNotEmpty,
+}) => {
+  const [todoTitle, setTodoTitle] = useState('');
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const trimmedTitle = todoTitle.trim();
+
+    const success = await onAddTodo(trimmedTitle);
+
+    if (success) {
+      setTodoTitle('');
+    }
+  };
+
+  useEffect(() => {
+    if (!isLoading) {
+      todoTitleRef?.current?.focus();
+    }
+  }, [todoTitleRef, isLoading]);
+
+  return (
+    <header className="todoapp__header">
+      {isTodoListNotEmpty && (
+        <button
+          type="button"
+          className={`todoapp__toggle-all ${isAllTodoCompleted ? 'active' : ''}`}
+          data-cy="ToggleAllButton"
+          onClick={onToggleAllTodos}
+        />
+      )}
+
+      <form onSubmit={onSubmit}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          ref={todoTitleRef}
+          value={todoTitle}
+          onChange={event => setTodoTitle(event.target.value)}
+          disabled={isLoading}
+        />
+      </form>
+    </header>
+  );
+};
