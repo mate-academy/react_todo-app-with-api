5904c601f9eea986ff06f79c6e5d527add2429d3
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-05-08 21:50:48 +0300

    test 4 solution

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,6 @@ export const App: React.FC = () => {
         if (anyRejected) {
           setError('Unable to delete a todo');
           setIsDeleteError('Unable to delete a todo');
-          throw new Error('Unable to delete a todo');
         }
 
         setTodos(prevTodos => {
