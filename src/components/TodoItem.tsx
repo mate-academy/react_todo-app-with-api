e13e2e0ebad1a6ccc1aad75d61840d51234c173b
Maciej Sikora <maciej.sikora1508@gmail.com>
2026-03-06 14:46:36 +0100

    add edit mode UI for todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import { Todo } from '../types/Todo';
 
@@ -19,7 +19,31 @@ export const TodoItem: React.FC<Props> = ({
   onDelete,
   onToggle,
 }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(todo.title);
+
+  const editFieldRef = useRef<HTMLInputElement>(null);
+
   const isLoading = isDeleting || isUpdating;
+
+  useEffect(() => {
+    setEditedTitle(todo.title);
+  }, [todo.title]);
+
+  useEffect(() => {
+    if (isEditing) {
+      editFieldRef.current?.focus();
+    }
+  }, [isEditing]);
+
+  const handleStartEditing = () => {
+    setEditedTitle(todo.title);
+    setIsEditing(true);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedTitle(event.target.value);
+  };
 
   return (
     <div
@@ -37,19 +61,39 @@ export const TodoItem: React.FC<Props> = ({
         />
       </label>
 
-      <span data-cy="TodoTitle" className="todo__title">
-        {todo.title}
-      </span>
+      {isEditing ? (
+        <form>
+          <input
+            ref={editFieldRef}
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            value={editedTitle}
+            onChange={handleTitleChange}
+          />
+        </form>
+      ) : (
+        <>
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={handleStartEditing}
+          >
+            {todo.title}
+          </span>
 
-      <button
-        type="button"
-        className="todo__remove"
-        data-cy="TodoDelete"
-        onClick={() => onDelete(todo.id)}
-        disabled={isDeleting}
-      >
-        ×
-      </button>
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={() => onDelete(todo.id)}
+            disabled={isDeleting}
+          >
+            ×
+          </button>
+        </>
+      )}
 
       <div
         data-cy="TodoLoader"
