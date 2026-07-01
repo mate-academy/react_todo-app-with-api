a01c8a7631a3174a8a434a44ebdec7ba20733130
Vhatever <fserrorka@gmail.com>
2026-07-01 16:26:26 +0300

    solution

---
+++ src/components/todo/TodoItem.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Todo } from '../../types/Todo';
+import classNames from 'classnames';
+
+type Props = {
+  todo: Todo;
+  isProcessed: boolean;
+  onDelete: () => void;
+  onUpdate: (data: Partial<Todo>) => Promise<void>;
+  dataCy?: string;
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  isProcessed,
+  onDelete,
+  dataCy,
+  onUpdate,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(todo.title);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const isSaving = useRef(false);
+  const editInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditing) {
+      editInputRef.current?.focus();
+    }
+  }, [isEditing]);
+
+  const handleUpdateTitle = async () => {
+    if (!isEditing || isSaving.current) {
+      return;
+    }
+
+    const trimmedTitle = newTitle.trim();
+
+    if (trimmedTitle === todo.title) {
+      setIsEditing(false);
+
+      return;
+    }
+
+    if (!trimmedTitle) {
+      onDelete();
+
+      return;
+    }
+
+    isSaving.current = true;
+    setIsUpdating(true);
+
+    try {
+      await onUpdate({ title: trimmedTitle });
+      setIsEditing(false);
+    } catch {
+      setIsUpdating(false);
+      editInputRef.current?.focus();
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  const handleKeyUp = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      handleUpdateTitle();
+    }
+
+    if (e.key === 'Escape') {
+      setNewTitle(todo.title);
+      setIsEditing(false);
+    }
+  };
+
+  return (
+    <div
+      data-cy={dataCy ?? 'Todo'}
+      className={classNames('todo', { completed: todo.completed })}
+    >
+      <label className="todo__status-label" aria-label="Toggle todo status">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+          onChange={() => onUpdate({ completed: !todo.completed })}
+        />
+      </label>
+
+      {isEditing ? (
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            handleUpdateTitle();
+          }}
+        >
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            ref={editInputRef}
+            value={newTitle}
+            onChange={e => setNewTitle(e.target.value)}
+            onKeyUp={handleKeyUp}
+            onBlur={handleUpdateTitle}
+            disabled={isUpdating || isProcessed}
+          />
+        </form>
+      ) : (
+        <>
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={() => setIsEditing(true)}
+          >
+            {todo.title}
+          </span>
+
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={onDelete}
+          >
+            ×
+          </button>
+        </>
+      )}
+
+      <div
+        data-cy="TodoLoader"
+        className={classNames('modal overlay', { 'is-active': isProcessed })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
---
+++ src/types/FilterOption.ts
@@ -0,0 +1,5 @@
+export enum FilterOption {
+  Active = 'Active',
+  Completed = 'Completed',
+  default = 'Default',
+}
